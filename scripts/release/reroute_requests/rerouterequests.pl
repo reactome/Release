9c7d76bb31146d89c3f2360ee246eecbd3c14227
Joel Weiser <weiserj@reactomerelease.(none)>
2014-07-09 12:22:03 -0400

    removed use of Net::OpenSSH
    Former-commit-id: 949168434abcb299e921d5d4d747d6258d797e08

--- scripts/release/reroute_requests/rerouterequests.pl
+++ scripts/release/reroute_requests/rerouterequests.pl
@@ -7,7 +7,6 @@ use warnings;
 # Author: Joel Weiser (joel.weiser@oicr.on.ca)
 
 use autodie;
-use Carp;
 
 my $port = shift;
 ($port && $port =~ /^\d+$/) || die "$0: Port number is required as the first argument to this script\n" . usage_instructions();
@@ -45,7 +44,7 @@ sub redirect_requests_in_configuration_file {
     } else {
         `cat $new_config_file_contents > $config_file`;
     }
-} 
+}
 
 sub get_configuration_file_contents {
     my $config_file = shift;
@@ -54,28 +53,6 @@ sub get_configuration_file_contents {
     my $contents = $remote_server ? `ssh $remote_server 'cat $config_file'` : `cat $config_file'`;
     
     return split /\n/, $contents ;
-}
-
-sub get_file_handle {
-    my $mode = shift; # Reading or writing
-    my $file = shift;
-    my $remote_server = shift;
-    
-    my ($file_handle, $pid);
-    if ($remote_server) {
-        my $ssh = Net::OpenSSH->new($remote_server);
-        $ssh->error and croak $ssh->error;
-        
-        if ($mode eq ">") {
-            ($file_handle, $pid) = $ssh->pipe_in("cat >$file") or croak $ssh->error; # Writing
-        } else {
-            ($file_handle, $pid) = $ssh->pipe_out("cat $file") or croak $ssh->error; # Reading
-        }
-    } else {
-        open($file_handle, $mode, $file);
-    }
-    
-    return $file_handle;
 }
 
 sub usage_instructions {
