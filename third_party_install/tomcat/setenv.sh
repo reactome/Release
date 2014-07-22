068694d3a38cfbded7139751cab3dbb66021e1df
smckay <smckay@reactomerelease.(none)>
2014-07-21 21:23:59 -0400

    "Third party install files"
    Former-commit-id: 30199add53e9e6972de4c4da03a97f1636f4c18a

---
+++ third_party_install/tomcat/setenv.sh
@@ -0,0 +1,8 @@
+
+export JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF-8 -Djava.awt.headless=true"
+
+export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
+echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+
+
+
