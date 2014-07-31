d4f5e82515018c29273a0a687eae9103c114891c
Sheldon McKay <sheldon.mckay@gmail.com>
2014-07-30 20:25:11 -0400

    fix merge error

--- website/html/wordpress/wp-config.php
+++ website/html/wordpress/wp-config.php
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-<?php
-/** 
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information by
- * visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
-
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
-
-/** MySQL database username */
-define('DB_USER', 'blogger');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'b10gg3r!');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-=======
 <?php
 /** 
  * The base configurations of the WordPress.
@@ -55,7 +19,6 @@ define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
->>>>>>> ed03efb86776463a35d8e2f22accc277a0a7b268
 
 /**#@+
  * Authentication Unique Keys.
