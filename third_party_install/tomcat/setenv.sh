e4a4f8204aff1b42213e5fd6fab79dc1a24ebef7
Sheldon McKay <sheldon.mckay@gmail.com>
2014-08-03 06:10:35 -0400

    third party install cleanup
    Former-commit-id: 90a11ae33e982a08b0cf8114a270611f0044522f

--- third_party_install/tomcat/setenv.sh
+++
@@ -1,8 +0,0 @@
-
-export JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF-8 -Djava.awt.headless=true"
-
-export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
-echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH
-
-
-
