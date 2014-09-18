43bd400c4db216fa092d85dad385ed55277052a7
Sheldon McKay <sheldon.mckay@gmail.com>
2014-09-18 10:19:53 -0400

    biopax update
    Former-commit-id: ae5b71b22a5ae0f4e6cb26610433de43da78ffdc

---
+++ scripts/release/download_directory/run_biopax.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+use strict;
+
+my ($host,$db,$user,$pass,$port,$release) = @ARGV;
+$host && $db && $user && $pass && $port && $release || die "Usage: ./run_biopax.pl host db user pass port releasenum\n";
+
+
+chomp(my $cwd = `pwd`);
+
+chdir 'biopaxexporter' or die $!;
+run_or_die("./runAllSpecies.sh $host $db $user $pass $port $release");
+chdir '../biopaxvalidator' or die $!;
+run_or_die("./validate_biopax.pl");
+chdir '../biopaxexporter/$release' or die $!;
+run_or_die("zip biopax2 *.owl");
+run_or_die("zip biopax2_validator *.xml");
+chdir $cwd or die $!;
+run_or_die("mv biopaxexporter/$release/biopax*.zip $release");
+
+chdir 'biopaxexporter' or die $!;
+run_or_die("./runAllSpeciesLevel3.sh $host $db $user $pass $port $release");
+chdir '../biopaxvalidator' or die $!;
+run_or_die("./validate_biopax.pl");
+chdir '../biopaxexporter/$release' or die $!;
+run_or_die("zip biopax *.owl");
+run_or_die("zip biopax_validator *.xml");
+chdir $cwd or die $!;
+run_or_die("mv biopaxexporter/$release/biopax*.zip $release");
+
+sub run_or_die {
+    my $cmd = shift;
+    my $retval = system $cmd;
+    die $! if $retval;
+}
