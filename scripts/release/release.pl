b11e6964560914c74aee8a63532216f8ad0ed0d6
Joel Weiser <weiserj@reactomerelease.(none)>
2014-10-02 15:15:13 -0400

    download directory creation moved after skypainter and orthodiagrams

--- scripts/release/release.pl
+++ scripts/release/release.pl
@@ -10,23 +10,23 @@ use constant {
 };
 
 my @options = (
-	['UniprotUpdate',"gkbdev","-runs uniprot update"],
-	['GoUpdate', "gkbdev", "-runs go update"],
-    	['ChebiUpdate', "gkbdev", "-runs chebi update"],
-    	['Orthopair', "gkbdev", "-runs orthopair script"],
-    	['UpdateSourceCode', "gkbdev", "-updates source code"],
-    	['ClearData', "gkbdev", "-clear data from a previous run \[NB: do not do this if this is the first run\]"],
-    	['GenerateStableIds', "gkbdev", "-generates stable ids"],
-    	['GoaPrepare', "gkbdev", "-prepares GOA submission file"],
-    	['Myisam', "gkbdev", "-convert database to myisam format"],
-    	['OrthoInference', "gkbdev", "-computational prediction for other species"],
-    	['UpdateConfig', "gkbdev", "-update configuration file to current version"],
-    	['ExternalResourceLinks', "gkbdev", "-creates external resource links"],
-    	['GenerateOrthoInferenceStableIds', "gkbdev", "-assign stable identifiers to ortho-predicted instances"],
-    	['DownloadDirectory', "gkbdev", "-update the download directory"],
-    	['Skypainter', "gkbdev", "-creates skypainter database"],
-    	['FrontpageAndTOC', "gkbdev", "-create frontpage images and cached toc file"],
-    	['Orthodiagrams', "gkbdev", "-create diagrams for predicted pathways"],
+	['UniprotUpdate', GKB_DEV_ALIAS,"-runs uniprot update"],
+	['GoUpdate', GKB_DEV_ALIAS, "-runs go update"],
+    	['ChebiUpdate', GKB_DEV_ALIAS, "-runs chebi update"],
+    	['Orthopair', GKB_DEV_ALIAS, "-runs orthopair script"],
+    	['UpdateSourceCode', GKB_DEV_ALIAS, "-updates source code"],
+    	['ClearData', GKB_DEV_ALIAS, "-clear data from a previous run \[NB: do not do this if this is the first run\]"],
+    	['GenerateStableIds', GKB_DEV_ALIAS, "-generates stable ids"],
+    	['GoaPrepare', GKB_DEV_ALIAS, "-prepares GOA submission file"],
+    	['Myisam', GKB_DEV_ALIAS, "-convert database to myisam format"],
+    	['OrthoInference', GKB_DEV_ALIAS, "-computational prediction for other species"],
+    	['UpdateConfig', GKB_DEV_ALIAS, "-update configuration file to current version"],
+    	['ExternalResourceLinks', GKB_DEV_ALIAS, "-creates external resource links"],
+    	['GenerateOrthoInferenceStableIds', GKB_DEV_ALIAS, "-assign stable identifiers to ortho-predicted instances"],    	
+    	['Skypainter', GKB_DEV_ALIAS, "-creates skypainter database"],
+    	['FrontpageAndTOC', GKB_DEV_ALIAS, "-create frontpage images and cached toc file"],
+    	['Orthodiagrams', GKB_DEV_ALIAS, "-create diagrams for predicted pathways"],
+	['DownloadDirectory', GKB_DEV_ALIAS, "-update the download directory"],
     	['RerouteRequests', GKB_LIVE_ALIAS, "-reroute website to port 8000"],
     	['UpdateSourceCode', GKB_LIVE_ALIAS, "-update source code"],
     	['DownloadDirectory', GKB_LIVE_ALIAS, "-update download directory"],
@@ -37,17 +37,17 @@ my @options = (
     	['ClearSearchCache', GKB_LIVE_ALIAS, "-clear search cache"],
     	['RerouteRequests', GKB_LIVE_ALIAS, "-switch back to public server"],
     	['RestartTomcat', GKB_LIVE_ALIAS, "-restart tomcat for WS SOAP API"],
-    	['CommitGoa', "gkbdev", "-commit goa files to cvs"],
-    	['NCBI', "gkbdev", "-create gene,protein, and omim files as well as hapmap and ucsc"],
-    	['SetEmailReminders',"gkbdev","-sets automation of e-mail reminders for next release"],
-    	['EventsAndEntities', "gkbdev", "-creates reaction and pathway to/from genes and molecules"]
+    	['CommitGoa', GKB_DEV_ALIAS, "-commit goa files to cvs"],
+    	['NCBI', GKB_DEV_ALIAS, "-create gene,protein, and omim files as well as hapmap and ucsc"],
+    	['SetEmailReminders', GKB_DEV_ALIAS,"-sets automation of e-mail reminders for next release"],
+    	['EventsAndEntities', GKB_DEV_ALIAS, "-creates reaction and pathway to/from genes and molecules"]
 );
 
 
 my @choices;
 my @allchoices;
 
-if ($ARGV[0] !~ /\d(\.\.)\d|\d,?/) {
+unless (defined $ARGV[0] && $ARGV[0] =~ /\d(\.\.)\d|\d,?/) {
     print "Usage: perl $0 'numbers'\n";
     print "Example: perl $0 1..3,4,12\n\n";
     print "Choose numbers from the following list:\n";
@@ -61,36 +61,36 @@ if ($ARGV[0] !~ /\d(\.\.)\d|\d,?/) {
     }
     
     exit;
-} else { 
-	@choices = split ',', $ARGV[0];
-	foreach my $choice (@choices) {
-    		if ($choice =~ /\.\./) {
-				my ($start, $end) = split '..', $choice;
-        		for (my $i = $start; $i <= $end; $i++) {
-            			$allchoices[$i] = 1;
-        		}
-    		} else {
-        		$allchoices[$choice] = 1; 
-    		}
-	}	
-	my @instances;
-	for (my $i = 1; $i <= scalar @allchoices; $i++) {
-		if ($allchoices[$i]) {     		
-   			my $index = $i - 1;
-   			my $step = $options[$index];
-    
-   			my $gkbdir = $step->[1]; # gkbdev, gkb_prod, or gkb
-		                        
-  			my $class = 'GKB::Release::Steps::' . $step->[0];
-			eval "require $class" or die;
-  			my $instance = $class->new('gkb' => $gkbdir);
-			$instance->set_user_input_and_passwords();
-			push @instances, $instance;
-		}    
-	}
-	
-	foreach my $instance (@instances) {
-		$instance->run();
-	}	
 }
 
+@choices = split ',', $ARGV[0];
+foreach my $choice (@choices) {
+    if ($choice =~ /\.\./) {
+	my ($start, $end) = split '..', $choice;
+        for (my $i = $start; $i <= $end; $i++) {
+            $allchoices[$i] = 1;
+        }
+    } else {
+        $allchoices[$choice] = 1; 
+    }
+}	
+
+my @instances;
+for (my $i = 1; $i <= scalar @allchoices; $i++) {
+    if ($allchoices[$i]) {     		
+   	my $index = $i - 1;
+   	my $step = $options[$index];
+    
+   	my $gkbdir = $step->[1]; # gkbdev or gkb
+		                        
+  	my $class = 'GKB::Release::Steps::' . $step->[0];
+	eval "require $class" or die;
+  	my $instance = $class->new('gkb' => $gkbdir);
+	$instance->set_user_input_and_passwords();
+	push @instances, $instance;
+    }    
+}
+
+foreach my $instance (@instances) {
+    $instance->run();
+}	
