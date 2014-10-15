b32834c7399a4c5a160bbaea5fe7211151131ced
root <root@reactomerelease.(none)>
2014-10-14 20:15:21 -0400

    release module updates

--- modules/GKB/Release/Steps/UpdateSourceCode.pm
+++ modules/GKB/Release/Steps/UpdateSourceCode.pm
@@ -8,7 +8,7 @@ extends qw/GKB::Release::Step/;
 
 has '+gkb' => ( default => "gkbdev" );
 has '+passwords' => ( default => sub { ['sudo'] });
-has '+directory' => ( default => "$release" );
+has '+directory' => ( default => "$release/update_source_code" );
 has '+mail' => ( default => sub { 
 					my $self = shift;
 					return {
@@ -21,10 +21,19 @@ has '+mail' => ( default => sub {
 );
 
 override 'run_commands' => sub {
-	my ($self, $gkbdir) = @_;
-     
-    cmd("Updating source code from git",[["./git_update.pl"]]); 
-
+    my ($self, $gkbdir) = @_;
+    
+        
+    # The command is run on the live server when $gkbdir is gkb
+    my $ssh_server = ($gkbdir eq "gkb") ? $live_server : undef;
+   
+    cmd("Updating source code from git",[
+                                         ["echo $sudo | sudo -S git stash"],
+                                         ["echo $sudo | sudo -S git pull"],
+                                         ["echo $sudo | sudo -S git stash pop"]
+                                        ],
+	{'ssh' => $ssh_server}
+    );
 };
 
 1;
