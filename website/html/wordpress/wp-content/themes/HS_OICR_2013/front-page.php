d854db79d5131b4b16b9ec25158c3758c660e5e8
Sheldon McKay <sheldon.mckay@gmail.com>
2014-10-16 07:05:25 -0400

    catching up prd1

--- website/html/wordpress/wp-content/themes/HS_OICR_2013/front-page.php
+++ website/html/wordpress/wp-content/themes/HS_OICR_2013/front-page.php
@@ -35,7 +35,7 @@
     <div class="contentwrap">
     <div class="contenthead">About Reactome</div><!--close contenthead-->
     <div class="contentbody">
-    	Reactome is a free, open-source, curated and peer reviewed pathway database. Our goal is to provide intuitive bioinformatics tools for the visualization, interpretation and analysis of pathway knowledge to support basic research, genome analysis, modeling, systems biology and education. The current version (v49) of Reactome was released on July 15, 2014.
+    	Reactome is a free, open-source, curated and peer reviewed pathway database. Our goal is to provide intuitive bioinformatics tools for the visualization, interpretation and analysis of pathway knowledge to support basic research, genome analysis, modeling, systems biology and education. The current version (v50) of Reactome was released on October 8, 2014.
     </div><!--close contentbody-->
     </div><!--close contentwrap-->
     
@@ -51,11 +51,11 @@
     <div class="grid_9"> 
             <div class="contentwrap">
             <div class="contenthead">
-            Reactome News </div>
+            Tweets  </div>
             <div class="contentbody">
             
             <div class="announcement">
-            <a href="/?p=1841"> New for 2014: Version 49 Released
+            <a href="/after-10-years-reactome-hits-50/"> Current Version: Reactome hits 50
             <!--This is a placeholder for static announcements. For example, downtime due to server maintenance, or some other important announcement-->
             </div><!--close annoucement-->
 
