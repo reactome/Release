518613fda47a90b67189ab79991c1748de90851f
Sheldon McKay <sheldon.mckay@gmail.com>
2014-11-07 02:08:30 -0500

    hopefully the slicing tool is good to go now

--- slicingTool/runProjectSlicingTool.sh
+++ slicingTool/runProjectSlicingTool.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
-java -Xmx8G -jar ProjectSlicingTool.jar >& out.txt &
+
+# Make sure we have all of the necessary params
+LOG=$(grep    logFileName slicingTool.prop | perl -pe 's/^\S+=//');
+DB=$(grep   slicingDbName slicingTool.prop | perl -pe 's/^\S+=//');
+USER=$(grep slicingDbUser slicingTool.prop | perl -pe 's/^\S+=//');
+PASS=$(grep  slicingDbPwd slicingTool.prop | perl -pe 's/^\S+=//');
+
+if [[ -n $DB && -n $USER && -n $PASS ]]
+then
+  echo Dropping $DB ...
+  mysql -u$USER -p$PASS -e "drop database if exists $DB"
+else
+  echo -e "\nMissing parameters!  Please complete slicingTool.prop\n"
+  exit
+fi
+
+echo "
+I am going to echo the output to the screen below
+
+Hit Control-C to exit when you are done...
+
+Running ProjectSlicingTool..."
+
+sleep 5
+
+screen -d -m java -Xmx8G -jar ProjectSlicingTool.jar
+
+tail -f SlicingTool.log $LOG
+
+exit 0;
+
+
