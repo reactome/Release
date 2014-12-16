f9422a070d92328023ed085bb14ec99e5a3580c1
Joel Weiser <weiserj@reactomerelease.(none)>
2014-12-16 10:58:08 -0500

    update source code for git

--- scripts/release/update_source_code/git_update.pl
+++ scripts/release/update_source_code/git_update.pl
@@ -2,6 +2,21 @@
 use strict;
 use warnings;
 
-`git stash`;
-`git pull`;
-`git stash pop`;
+my $git_repo = shift || die usage_instructions();
+my $ssh_server = shift;
+
+my $update_cmd = "cd $git_repo;git stash;git pull;git stash pop";
+
+$ssh_server ? `ssh $ssh_server $update_cmd` : `$update_cmd`;
+
+sub usage_instructions {
+    return <<END;
+
+Please provide a git repository directory and, optionally, a host name
+
+Usage: perl $0 git_repo [server host]
+Examples: perl $0 /usr/local/gkb
+	  perl $0 /usr/local/gkb reactomeprd1.oicr.on.ca
+END
+
+}
