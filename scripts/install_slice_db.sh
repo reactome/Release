5cb7b28c9078b1f7f7d3c94e69e9332faa76baac
Sheldon McKay <sheldon.mckay@gmail.com>
2015-02-23 04:33:41 -0500

    removed root requirement

--- scripts/install_slice_db.sh
+++ scripts/install_slice_db.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#SEARCH=/usr/local/reactomes/Reactome/production/apache-tomcat/webapps/content/WEB-INF/classes/web.properties
 API=/usr/local/reactomes/Reactome/production/apache-tomcat/webapps/ReactomeRESTfulAPI/WEB-INF/applicationContext.xml
 WEB=/usr/local/reactomes/Reactome/production/GKB/modules/GKB/Secrets.pm
 
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root!" 1>&2
-    exit 1
-fi
+#if [[ $EUID -ne 0 ]]; then
+#    echo "This script must be run as root!" 1>&2
+#    exit 1
+#fi
 
 db_name=$1
 if [[ ! -n $db_name ]]; then 
     echo "Usage ./install_slice_db.pl slice_db_name" 1>&2
     exit 1
 fi
+
 
 perl -i -pe "s/<constructor-arg index=\"1\" value=\"\w+\"/<constructor-arg index=\"1\" value=\"${db_name}\"/" $API
 AOK=`grep $db_name $API`
@@ -31,17 +31,6 @@ if [[ ! -n $BOK ]]; then
     exit 1
 fi
 
-perl -i -pe "s/database_currentDatabase=\S+/database_currentDatabase=$db_name/" $SEARCH;
-COK=`grep $db_name $SEARCH`
-echo $COK
-if [[ ! -n $COK ]]; then
-    echo "Database renaming for $SEARCH failed" 1>&2
-    exit 1
-fi
-
-
-# restart tomcat
-/etc/init.d/tomcat7 restart
 
 
 
