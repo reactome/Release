41a83960306709f9acfe54a830fccf65a624b7e5
Sheldon McKay <sheldon.mckay@gmail.com>
2015-03-10 08:24:23 -0400

    removed sudo requirement for deply.pl

--- scripts/tomcat_staging/webapps/deploy.pl
+++ scripts/tomcat_staging/webapps/deploy.pl
@@ -5,7 +5,7 @@ my $only = shift;
 
 while (my $war = <*.war>) {
     next if $only && $war !~ /$only/i;
-    system "sudo -u tomcat7 cp $war /usr/local/reactomes/Reactome/production/apache-tomcat/webapps";
+    system "cp $war /usr/local/reactomes/Reactome/production/apache-tomcat/webapps";
 }
 
 
