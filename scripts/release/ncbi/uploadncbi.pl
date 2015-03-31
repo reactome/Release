910bf1eaed0a6e1c187e46d0feb5bcbd4c3bd31d
Joel Weiser <weiserj@reactomerelease.(none)>
2015-03-31 16:39:09 -0400

    upload new files and remove old files

--- scripts/release/ncbi/uploadncbi.pl
+++ scripts/release/ncbi/uploadncbi.pl
@@ -4,14 +4,20 @@ use warnings;
 use v5.10;
 use Net::FTP;
 
-my $ftp = Net::FTP->new("ftp-private.ncbi.nih.gov", DEBUG => 0);
+my $ftp = Net::FTP->new("ftp-private.ncbi.nih.gov", Debug => 0, Passive => 1);
 my ($ftppass, $version) = @ARGV;
+my $previous_version = $version - 1;
 
 $ftp->login("reactome", $ftppass) or die "NCBI login failed";
 $ftp->cwd("holdings");
+
 $ftp->put("archive/gene_reactome$version.xml");
+$ftp->delete("gene_reactome$previous_version.xml");
 $ftp->put("archive/protein_reactome$version.ft");
+$ftp->delete("protein_reactome$previous_version.ft");
 $ftp->put("archive/omim_reactome$version.ft");
+$ftp->delete("omim_reactome$previous_version.ft");
+
 say 'The following files are in the NCBI holdings directory';
 say foreach $ftp->ls();
 $ftp->quit();
