97bbbdf30c7d54fae68caef0f193a93c3ef1fd9b
Joel Weiser <weiserj@reactomerelease.(none)>
2015-04-07 19:43:40 -0400

    EnsEMBL utility modules

---
+++ modules/GKB/EnsEMBLMartUtils.pm
@@ -0,0 +1,66 @@
+package GKB::EnsEMBLMartUtils;
+
+use strict;
+use warnings;
+
+use constant REGISTRY_FILE => 'registry.xml';
+
+use lib "/usr/local/reactomes/Reactome/development/GKB/modules";
+use lib '/usr/local/reactomes/Reactome/development/GKB/BioMart/biomart-perl/lib';
+
+use BioMart::Initializer;
+use BioMart::Query;
+use BioMart::QueryRunner;
+
+use parent 'GKB::EnsEMBLUtils';
+
+our @EXPORT_OK = qw/get_query get_query_runner update_registry_file get_identifiers get_version get_ensembl_genome_version/;
+my %EXPORT_TAGS = (all => [qw/get_query get_query_runner update_registry_file get_identifiers get_version get_ensembl_genome_version/],
+                   query => [qw/get_query get_query_runner/]);
+
+sub get_query {
+    my $action = shift // 'cached';
+    
+    my $initializer = BioMart::Initializer->new('registryFile'=>REGISTRY_FILE,'action'=>$action);
+    my $registry = $initializer->getRegistry;
+
+    return BioMart::Query->new('registry'=>$registry,'virtualSchemaName'=>'default');
+}    
+
+sub get_query_runner {
+    return BioMart::QueryRunner->new();
+}
+
+sub update_registry_file {
+    my $registry_file = shift // REGISTRY_FILE;
+    
+    my $version = get_version();
+    return unless $version =~ /^\d+$/;
+    
+    my $contents = `cat $registry_file`;
+    chomp $contents;
+    $contents =~ s/(ensembl_mart_)(\d+)/$1$version/;    
+    
+    my $update = $version != $2;
+    `echo '$contents' > $registry_file` if $update;
+    `rm -r *[Cc]ached*/` if $update;
+    
+    return $update;
+}
+
+sub get_identifiers {
+    my $species = shift;
+    my $ensembl_url = 'http://www.ensembl.org/biomart/martservice?type=listAttributes&mart=ENSEMBL_MART_ENSEMBL&virtualSchema=default&dataset='.$species.'_gene_ensembl&interface=default&attributePage=feature_page&attributeGroup=external&attributeCollection=';
+    
+    my @identifiers;
+    
+    foreach my $attribute_type ('xrefs', 'microarray') {
+        my $url = $ensembl_url.$attribute_type;
+        my $results = `wget -qO- '$url'`;
+        push @identifiers, (split /\n/, $results);
+    }
+    
+    return @identifiers, "interpro", "smart", "pfam", "prints";
+}
+
+1;
---
+++ modules/GKB/EnsEMBLUtils.pm
@@ -0,0 +1,52 @@
+package GKB::EnsEMBLUtils;
+
+use strict;
+use warnings;
+
+use constant GKB_MODULES => '/usr/local/gkb/modules';
+
+use parent 'Exporter';
+
+our @EXPORT_OK = qw/get_version get_ensembl_genome_version install_ensembl_api ensembl_api_installed/;
+my %EXPORT_TAGS = (all => [qw/get_version get_ensembl_genome_version install_ensembl_api ensembl_api_installed/],);
+
+sub get_version {
+    my $url = 'http://rest.ensembl.org/info/software?';
+    
+    my $release_json = `wget -q --header='Content-type:application/json' '$url' -O -`;
+    
+    my ($release) = $release_json =~ /"release":(\d+)/;
+    
+    return $release - 1;
+}
+
+sub get_ensembl_genome_version {
+    my $url = 'http://rest.ensemblgenomes.org/info/eg_version?';
+    
+    my $version_json = `wget -q --header='Content-type:application/json' '$url' -O -`;
+    
+    my ($version) = $version_json =~ /"version":"(\d+)"/;
+    
+    return $version;
+}
+
+sub install_ensembl_api {
+    my ($self, $version) = @_;
+    
+    chdir GKB_MODULES;
+    `perl install_ensembl_api.pl $version`;
+    chdir $self->directory;
+}
+
+sub ensembl_api_installed {
+    my $ensembl_api_dir = GKB_MODULES . "/ensembl_api/";
+    my @subdirectories = qw/bioperl-live ensembl ensembl-compara/;
+    
+    foreach my $subdirectory (map {$ensembl_api_dir . $_} @subdirectories) {
+	return 0 unless (-d $subdirectory);
+    }
+    
+    return 1;
+}
+
+1;
