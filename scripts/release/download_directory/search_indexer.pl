6a868c4c84c0971b4783694119d282ea8450f59c
Sheldon McKay <sheldon.mckay@gmail.com>
2015-04-07 23:49:06 -0400

    security bypass for indexer

--- scripts/release/download_directory/search_indexer.pl
+++ scripts/release/download_directory/search_indexer.pl
@@ -2,10 +2,6 @@
 use strict;
 use common::sense;
 
-# I think it's more user-friendly if the user will not have to practise any
-# symlink tricks. Hence the BEGIN block. If the location of the script or
-# libraries changes, this will have to be changed.
-
 BEGIN {
     my @a = split('/',$0);
     pop @a;
@@ -21,13 +17,19 @@ use autodie;
 use Cwd;
 use Getopt::Long;
 
+use Log::Log4perl qw/get_logger/;
+
+use constant CONF => '/usr/local/reactomes/Reactome/development/apache-tomcat/webapps/solr/WEB-INF/web.xml';
+
+Log::Log4perl->init(\$LOG_CONF);
+my $logger = get_logger(__PACKAGE__);
+
 $GKB::Config::NO_SCHEMA_VALIDITY_CHECK = undef;
 
 our($opt_host,$opt_db,$opt_pass,$opt_port,$opt_debug,$opt_user,$opt_r);
 
-
-# WE need maven 3, die otherwise
-die "No maven3 installed, sorry I can't run" unless `which mvn3`;
+# We need maven 3, die otherwise
+$logger->error_die("No maven3 installed, sorry I can't run") unless `which mvn3`;
 
 my $usage = "Usage: $0 -db db_name -user db_user -host db_host -pass db_pass -port db_port -r release_number";
 
@@ -48,34 +50,49 @@ $opt_pass ||= $GKB::Config::GK_DB_PASS;
 $opt_port ||= $GKB::Config::GK_DB_PORT;
 
 my $solr_url  = "http://reactomerelease.oicr.on.ca:7080/solr/reactome";
-my $solr_user = $GKB::Config::GK_SOLR_USER;
-my $solr_pass = $GKB::Config::GK_SOLR_PASS;
 my $solr_dir  = '/usr/local/reactomes/Reactome/production/Solr/cores';
+
+# No point, they don't work sjm 2015-04-07
+# my $solr_user = $GKB::Config::GK_SOLR_USER;
+# my $solr_pass = $GKB::Config::GK_SOLR_PASS;
+
 
 my $present_dir = getcwd();
 my $output = "$present_dir/ebeye.xml";
 my $prev_release = $opt_r - 1;
 
-# first, clear the path for the new index, if necessary
-if (-d "$solr_dir/reactome_v$prev_release") {
-    say STDERR "I am getting ready to index release $opt_r.  First I need to move $prev_release out of the way";
-    chdir $solr_dir or die "Could not cd to $solr_dir $!";
-    system "tar czvf reactome_v$prev_release.tar.gz reactome_v$prev_release";
-    system "mv reactome_v$prev_release reactome_v$opt_r";
-    system "chmod -R g+w reactome_v$opt_r";
-    system "perl -i -pe 's/reactome_v$prev_release/reactome_v$opt_r/' reactome_v$opt_r/core.properties";
-    chdir $present_dir or die "Could not cd to $present_dir $!";
-}
+disable_authentication();
 
 # then index.  This actually uses tomcat/solr to write to the filesystem!
-say STDERR "I am updating and compiling the indexer now...";
+$logger->info("I am updating and compiling the indexer now...");
 chdir "$GK_ROOT_DIR/scripts/release/download_directory/search/indexer";
 system("mvn clean package");
 system("ln -sf target/Indexer-1.0-jar-with-dependencies.jar indexer.jar");
-say STDERR "I am indexing now...";
-system("java -jar -Xms5120M -Xmx10240M indexer.jar -d $opt_db -u $opt_user -p $opt_pass -s $solr_url ".
-       "-e $solr_user -a $solr_pass -c src/main/resources/controlledvocabulary.csv -o $output -r $opt_r ");
+$logger->info("I am indexing now...");
+system("java -jar -Xms5120M -Xmx10240M indexer.jar -d $opt_db -u $opt_user ".
+       "-p $opt_pass -s $solr_url ".#-e $solr_user -a $solr_pass ".
+       "-c src/main/resources/controlledvocabulary.csv -o $output -r $opt_r");
 system("gzip -f $output");
 
-say "$0 has finished its job\n";
+chdir $solr_dir;
+system "tar czvf reactome_v$opt_r.tar.gz reactome";
 
+enable_authentication();
+
+$logger->info("$0 has finished its job\n");
+
+
+sub disable_authentication {
+    my $conf = CONF;
+    my $disabled = "$conf.security-disabled";
+    my $retval = system "cp $disabled $conf";
+    die "problem with $conf" if $retval;
+    sleep 60;
+}
+
+sub enable_authentication {
+    my $conf = CONF;
+    my $enabled = "$conf.security-enabled";
+    my $retval = system "cp $enabled $conf";
+    die "problem with $conf" if $retval;
+}
