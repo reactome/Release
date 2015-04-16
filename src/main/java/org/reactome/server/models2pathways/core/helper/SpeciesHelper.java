085825c1ea7c4dd21f9cbc7bb97730e3d811ac3e
Joel Weiser <weiserj@reactomerelease.(none)>
2015-04-16 11:24:00 -0400

    Squashed 'scripts/release/biomodels/repository/' content from commit ad7e8af
    git-subtree-dir: scripts/release/biomodels/repository
    git-subtree-split: ad7e8af94a20752882fb2c9fefb8d1ae07359dbd

---
+++ src/main/java/org/reactome/server/models2pathways/core/helper/SpeciesHelper.java
@@ -0,0 +1,46 @@
+package org.reactome.server.models2pathways.core.helper;
+
+import org.reactome.server.models2pathways.core.model.Specie;
+
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Created by Maximilian Koch (mkoch@ebi.ac.uk).
+ */
+public class SpeciesHelper {
+
+    private static SpeciesHelper speciesHelper = new SpeciesHelper();
+    private Set<Specie> species;
+
+    private SpeciesHelper() {
+    }
+
+    public static SpeciesHelper getInstance() {
+        return speciesHelper;
+    }
+
+    public Specie getSpecieByBioMdSpecieId(Long id) {
+        for (Specie specie : species) {
+            if (Objects.equals(specie.getBioMdId(), id)) {
+                return specie;
+            }
+        }
+        return null;
+    }
+
+    public Set<Specie> getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(Set<Specie> species) {
+        this.species = species;
+    }
+
+    @Override
+    public String toString() {
+        return "SpeciesHelper{" +
+                "species=" + species +
+                '}';
+    }
+}
