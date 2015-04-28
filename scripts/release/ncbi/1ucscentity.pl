81e63b6d1beb9c5ee55af1c633306df478377fcd
Joel Weiser <weiserj@reactomerelease.(none)>
2015-04-28 13:43:22 -0400

    1ucscentity.pl refactored

--- scripts/release/ncbi/1ucscentity.pl
+++ scripts/release/ncbi/1ucscentity.pl
@@ -2,15 +2,13 @@
 use strict;
 
 use lib '/usr/local/gkb/modules';
-use lib "$ENV{HOME}/bioperl-1.0";
-#use lib "$ENV{HOME}/GKB/modules";
+
 use GKB::ClipsAdaptor;
 use GKB::DBAdaptor;
+
+use autodie;
 use Data::Dumper;
 use Getopt::Long;
-
-my $count =0;
-my $count1 =0;
 
 our ( $opt_user, $opt_host, $opt_pass, $opt_port, $opt_db);
 (@ARGV)  || die "Usage: $0 -user db_user -host db_host -pass db_pass -port db_port -db db_name\n";
@@ -22,34 +20,7 @@ my ($num) = $opt_db =~ /(\d+)/;
 my $entity_base_url = "http://www.reactome.org/content/query?q=UniProt:";
 my $event_base_url = "http://www.reactome.org/PathwayBrowser/#";
 
-my $archive = 'archive/';
-my $out_put = $archive . 'ucsc_errorfile.txt';
-
-my $out_put2 = $archive . 'ucsc_events'.$num;
-
-my $out_put3 = $archive . 'ucsc_entity'.$num;
-
-open (OUTPUT, ">$out_put");
-
-open (OUTPUT2, ">$out_put2"); 
-
-open (OUTPUT3, ">$out_put3");
-
-print OUTPUT2  "URL for events: $event_base_url\n\n";
-
-print OUTPUT2  "Reactome Entity\tEvent ST_ID\tEvent_name\n\n";
-
-print OUTPUT3 "URL for entity_identifier: $entity_base_url\n\n";
-
-print OUTPUT3  "Reactome Entity\n\n";
-
-
-my @out_classes = ('Event');
-
-
-my %hash;
-
-my $dbid;
+my $archive = 'archive';
 
 my $dba = GKB::DBAdaptor->new(
      -dbname => $opt_db,
@@ -59,63 +30,81 @@ my $dba = GKB::DBAdaptor->new(
      -port   =>  3306
 );
 
-# gets Entrez Gene
-    
-my $gene_db = $dba->fetch_instance_by_attribute('ReferenceDatabase', [['name', ['UniProt']]]) ||  die("No ReferenceDatabase with name 'UniProt'.\n");
-    
-#print Dumper( $gene_db);
-
-#print $gene_db->[0]->db_id."\n";
-
-
-my $s =0;
-my @input;
-my %test;
-my $species;
-my $xy =0;
+my @accepted_rgp_ids;
+my $rejected_rgp_count;
 
 my $protein_class = &GKB::Utils::get_reference_protein_class($dba);
+# gets Entrez Gene
+my $gene_db = $dba->fetch_instance_by_attribute('ReferenceDatabase', [['name', ['UniProt']]]) ||  die("No ReferenceDatabase with name 'UniProt'.\n");
+my $uniprot_rgps = $dba->fetch_instance($protein_class, [['referenceDatabase',[$gene_db->[0]->db_id]]]);
 
-my $gp = $dba->fetch_instance($protein_class, [['referenceDatabase',[$gene_db->[0]->db_id]]]);
+open (my $entities_fh, ">", "$archive/ucsc_entity$num");
+print $entities_fh "URL for entity_identifier: $entity_base_url\n\n";
+print $entities_fh "Reactome Entity\n\n";
 
-foreach (@{$gp}){
-   
-   my $sp = $gp->[$s]->species->[0]->_displayName->[0];
-   
-   #print $sp."\n";
-   
+foreach my $uniprot_rgp (@{$uniprot_rgps}){
+   my $sp = $uniprot_rgp->species->[0]->_displayName->[0];
    if ($sp =~ /Homo|Rattus|Mus/){
-      push (@input,$gp->[$s]->Identifier->[0]."\n");      
-      print OUTPUT3 $gp->[$s]->identifier->[0]."\t". get_stable_identifier($gp->[$s])."\n";
+      push (@accepted_rgp_ids,$uniprot_rgp->Identifier->[0]);      
+      print $entities_fh $uniprot_rgp->identifier->[0]."\t". get_stable_identifier($uniprot_rgp)."\n";
    }else{
-      $xy++;
+      $rejected_rgp_count++;
    }
-   $s++;
 } 
 
-my $l = @input;
- 
 #To get a list of proteins, print this array to another output file
-
-print "Total number of proteins:".$l."\n";
+print "Total number of proteins:". scalar @accepted_rgp_ids."\n";
 print "Second step starts now\n";
 
-my $sdi;
-my $sdis;
-my $entity = "Reactome Entity:";
-my $event = "Reactome Event:";
-my $tag2 = "Reactome ST_ID:";
+my $error_count;
+my $entity_w_event_count;
 
-foreach (@input) { s/\s//g;}
+open (my $error_fh, ">", "$archive/ucsc.err");
+open (my $events_fh, ">", "$archive/ucsc_events$num"); 
 
-   $sdis = $dba->fetch_instance(-CLASS => $protein_class,
-				-QUERY => [{-ATTRIBUTE => 'identifier',
-                                -VALUE => \@input                                          
-                                }]);
-#print $_."===>x"."\n";				  
-				  
+print $events_fh "URL for events: $event_base_url\n\n";
+print $events_fh "Reactome Entity\tEvent ST_ID\tEvent_name\n\n";
 
+foreach my $uniprot_rgp (@{get_rgp_instances_by_id(@accepted_rgp_ids)}) {
+   unless ($uniprot_rgp->Identifier->[0]) {
+      print $error_fh $uniprot_rgp->extended_displayName . " has no identifier!\n";
+      next;
+   }
 
+   my @reactions_and_pathways = get_reactions_and_pathways($uniprot_rgp);
+   unless (@reactions_and_pathways) {
+      print $error_fh "$entity_base_url",$uniprot_rgp->Identifier->[0]."\n".$uniprot_rgp->extended_displayName . " participates in Event(s) but no top Pathway can be found, i.e. there seem to be a pathway which contains or is an instance of itself.\n";
+      $error_count++;
+      next;
+   }
+   
+   foreach my $event (@reactions_and_pathways){
+      print $events_fh
+	       $uniprot_rgp->Identifier->[0]. "\t" .
+	       get_stable_identifier($event). "\t" .
+	       fix_name($event->name->[0]) ."\n";
+   }
+   $entity_w_event_count++;
+}
+print "Number of entities with events is  ".$entity_w_event_count."\n";
+print "Number of not those entities is: ".$rejected_rgp_count."\n";
+
+close($entities_fh);
+close($events_fh);
+close($error_fh);
+
+sub get_rgp_instances_by_id {
+   my @ids = @_;
+   
+   return $dba->fetch_instance(-CLASS => $protein_class,
+			      -QUERY => [{-ATTRIBUTE => 'identifier',
+                              -VALUE => \@ids                                         
+                              }]);
+}
+
+sub get_reactions_and_pathways {
+   my $instance = shift;
+   
    my %instructions = (-INSTRUCTIONS =>
 		    {
 			$protein_class => {'reverse_attributes' => [qw(referenceEntity referenceSequence)]},
@@ -123,127 +112,19 @@ foreach (@input) { s/\s//g;}
 			'CatalystActivity' => {'reverse_attributes' =>[qw(catalystActivity regulator)]},
 			'Event' => {'reverse_attributes' =>[qw(hasComponent hasMember hasEvent)]}
 		    },
-		    -OUT_CLASSES => \@out_classes
-		    );
+		    -OUT_CLASSES => ['Event']
+   );
 
-foreach $sdi (@{$sdis}) {
-   unless ($sdi->Identifier->[0]) {
-	print STDERR $sdi->extended_displayName . " has no identifier!\n";
-	next;
-    }
-    
-	 
-   my $str;
-   # print "====>".$sdi->Identifier->[0]."\n";
-
-    my $ar = $sdi->follow_class_attributes(%instructions);
-
-   if (@{$ar}) {
-       @{$ar} = grep {! @{$_->reverse_attribute_value('hasInstance')}} grep {! @{$_->reverse_attribute_value('hasComponent')}} @{$ar};
+   my $events = $instance->follow_class_attributes(%instructions);
+   if (@{$events}) {
+      @{$events} = grep {! @{$_->reverse_attribute_value('hasInstance')}} grep {! @{$_->reverse_attribute_value('hasComponent')}} @{$events};
    }
-       
-   if (@{$ar}){	 
-	 
-      my $label = 'reactions';
-      my @pathways = grep {$_->is_a('Pathway')} @{$ar};
-      my @reactions = grep {$_-> is_a('Reaction')} @{$ar};
-	   
-      if(@reactions){
-   	 @{$ar} = @reactions;
-		 
-		#foreach(@{$ar}){
-	 if (@{$ar} == 1) {
-	    $str = $ar->[0]->Name->[0];
-	 	
-	    fix_name($str);
-
-	    print OUTPUT2 $sdi->Identifier->[0]."\t";
-	 	
-	    print OUTPUT2 get_stable_identifier($ar->[0])."\t";
-		 
-	    print OUTPUT2 "$str\n";
-	   	
-	   	
-		 
-	 } else{
-	      
-            my $p = 0;
-	       
-	    foreach(@{$ar}){  
-	    	
-	       $str = $ar->[$p]->Name->[0];
-		 
-		 
-	       fix_name($str);
-
-	       print OUTPUT2 $sdi->Identifier->[0]."\t";
-	 
-	       print OUTPUT2 get_stable_identifier($ar->[$p])."\t"; 
-		
-	       print OUTPUT2 "$str\n";
-		 
-	        
-	       $p++; 
-	       
-	    }		
-	 }
-      }
-		
-	 
-      if(@pathways){
-         @{$ar} = @pathways;
-         if (@{$ar} == 1) {
-	    $str = $ar->[0]->Name->[0];
-   	       
-	    fix_name($str);
    
-	    print OUTPUT2  $sdi->Identifier->[0]."\t";
-	 	
-	    print OUTPUT2 get_stable_identifier($ar->[0])."\t";
-	 	 
-	    print OUTPUT2  "$str\n";
-	 	 
-	 } else{
-	      
-	    my $p = 0;
-	        
-	    foreach(@{$ar}){  
-	 	   	# use join to have an array of evenly split elements
-	       $str =$ar->[$p]->Name->[0];
-	    		
-	       fix_name($str);
-	 			
-	       print OUTPUT2 $sdi->Identifier->[0]."\t";
-	 	 
-	       print OUTPUT2 get_stable_identifier($ar->[$p])."\t"; 
-	  	
-	       print OUTPUT2 "$str\n";
-		  
-	       $p++;		     
-	    }	
-	 }	    	
-      }
-      $count1++;
-   }else{
-      print OUTPUT "$entity_base_url",$sdi->Identifier->[0]."\n".$sdi->extended_displayName . " participates in Event(s) but no top Pathway can be found, i.e. there seem to be a pathway which contains or is an instance of itself.\n";
-      $count++;
-   }
+   my @pathways = grep {$_->is_a('Pathway')} @{$events};
+   my @reactions = grep {$_-> is_a('Reaction')} @{$events};
+   
+   return (@reactions, @pathways);
 }	
-print "Number of entities with events is  ".$count1."\n";
-print "Number of not those entities is: ".$xy."\n";
-print STDERR  "total no.of errors:$count\n";
-      
-sub find_top_events {
-   my ($events) = @_;
-   my @out;
-   foreach my $e (@{$events}) {
-      @{$e->reverse_attribute_value('hasComponent')} && next;
-      @{$e->reverse_attribute_value('hasInstance')} && next;
-      push @out, $e;
-   }
-   return \@out;
-}	
-
 
 sub fix_name{
    my ($str)= @_;
