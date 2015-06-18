0c1b0a06374d4a28601901271f7c8fb68e7287db
Joel Weiser <weiserj@reactomerelease.(none)>
2015-06-18 17:25:49 -0400

    new release steps

---
+++ modules/GKB/Release/Steps/SearchIndexer.pm
@@ -0,0 +1,32 @@
+package GKB::Release::Steps::SearchIndexer;
+
+use GKB::Release::Config;
+use GKB::Release::Utils;
+
+use Moose;
+extends qw/GKB::Release::Step/;
+
+has '+gkb' => ( default => "gkbdev" );
+has '+passwords' => ( default => sub { ['mysql'] } );
+has '+directory' => ( default => "$release/search_indexer" );
+has '+mail' => ( default => sub { 
+					my $self = shift;
+					return {
+						'to' => '',
+						'subject' => $self->name,
+						'body' => "",
+						'attachment' => ''
+					};
+				}
+);
+						
+override 'run_commands' => sub {
+	my ($self, $gkbdir) = @_;
+	
+	my $host = $self->host;
+
+    my @args = ("-db", $db, "-host", $host, "-user", $user, "-pass", $pass);
+    cmd("Running search indexer",[["perl search_indexer.pl @args > search.out 2> search.err"]]);
+};
+
+1;
