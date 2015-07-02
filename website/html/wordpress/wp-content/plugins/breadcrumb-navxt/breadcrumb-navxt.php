7b7648ac50e80164fd02a808548370e98c1b7af5
Sheldon McKay <sheldon.mckay@gmail.com>
2015-07-02 16:35:42 -0400

    wordpress updates

--- website/html/wordpress/wp-content/plugins/breadcrumb-navxt/breadcrumb-navxt.php
+++ website/html/wordpress/wp-content/plugins/breadcrumb-navxt/breadcrumb-navxt.php
@@ -3,12 +3,12 @@
 Plugin Name: Breadcrumb NavXT
 Plugin URI: http://mtekk.us/code/breadcrumb-navxt/
 Description: Adds a breadcrumb navigation showing the visitor&#39;s path to their current location. For details on how to use this plugin visit <a href="http://mtekk.us/code/breadcrumb-navxt/">Breadcrumb NavXT</a>. 
-Version: 5.2.0
+Version: 5.2.2
 Author: John Havlik
 Author URI: http://mtekk.us/
 License: GPL2
-TextDomain: breadcrumb-navxt
-DomainPath: /languages/
+Text Domain: breadcrumb-navxt
+Domain Path: /languages
 */
 /*  Copyright 2007-2015  John Havlik  (email : john.havlik@mtekk.us)
 
@@ -60,7 +60,7 @@ $breadcrumb_navxt = NULL;
 //TODO change to extends mtekk_plugKit
 class breadcrumb_navxt
 {
-	const version = '5.2.0';
+	const version = '5.2.2';
 	protected $name = 'Breadcrumb NavXT';
 	protected $identifier = 'breadcrumb-navxt';
 	protected $unique_prefix = 'bcn';
