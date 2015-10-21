51418116f181ee2f9e428195f1a74ef9a4df1415
Joel Weiser <weiserj@reactomerelease.(none)>
2015-10-21 14:38:23 -0400

    added usage instructions and argument defaults

--- scripts/release/download_directory/st_id_2_uniprot.pl
+++ scripts/release/download_directory/st_id_2_uniprot.pl
@@ -5,19 +5,38 @@ use Data::Dumper;
 # print st_id/uniprot mapping file
 
 use lib '/usr/local/gkb/modules';
+
+use Getopt::Long;
+
+use GKB::Config;
 use GKB::DBAdaptor;
 
-@ARGV >= 3 or die "$0 user pass db [class]";
-my ($user, $pass, $db) = @ARGV;
+my ($user, $pass, $host, $db, $class, $help);
+GetOptions(
+    'user=s' => \$user,
+    'pass=s' => \$pass,
+    'host=s' => \$host,
+    'db=s' => \$db,
+    'class=s' => \$class,
+    'help' => \$help
+);
+
+if ($help) {
+    print usage_instructions();
+    exit;
+}
+
 
 my $dba = GKB::DBAdaptor->new(
-    -dbname  => $db,
-    -user    => $user,
-    -pass    => $pass
-    );
+    -dbname  => $db || $GKB::Config::GK_DB_NAME,
+    -user    => $user || $GKB::Config::GK_DB_USER,
+    -host    => $host || $GKB::Config::GK_DB_HOST,
+    -pass    => $pass || $GKB::Config::GK_DB_PASS,
+    -port    => 3306
+);
 
 my $sth = $dba->prepare('SELECT DB_ID FROM DatabaseObject WHERE _class = ?');
-$sth->execute($ARGV[3] || 'EntityWithAccessionedSequence');
+$sth->execute($class || 'EntityWithAccessionedSequence');
 my @db_ids;
 while (my $ary = $sth->fetchrow_arrayref) {
     push @db_ids, $ary->[0];
@@ -29,11 +48,30 @@ for my $db_id (@db_ids) {
     my $refseq = $instance->referenceEntity->[0];
     my $uniprot = $refseq->displayName;
     if ($uniprot && $uniprot =~ /^uniprot:/i) {
-	$uniprot =~ s/^[^:]+://;
-	($uniprot) = split(/\s+/, $uniprot);
-	say join("\t",$st_id,$uniprot);
+        $uniprot =~ s/^[^:]+://;
+        ($uniprot) = split(/\s+/, $uniprot);
+        say join("\t",$st_id,$uniprot);
     }
     
 }
 
-
+sub usage_instructions {
+    return <<END;
+    
+    This script outputs the mapping of instance
+    stable identifiers to the UniProt identifier
+    it references.
+    
+    Usage: perl $0
+    
+    Options:
+    
+    -user [db_user]         User for source database (default is $GKB::Config::GK_DB_USER)
+    -pass [db_pass]         Password for source database (default is password for $GKB::Config::GK_DB_USER user)
+    -host [db_host]         Host for source database (default is $GKB::Config::GK_DB_HOST)
+    -db [db_name]           Source database (default is $GKB::Config::GK_DB_NAME)
+    -class [instance_class] Class of database objects to select (default is EntityWithAccessionedSequence)
+    -help                   Prints these instructions
+    
+END
+}
