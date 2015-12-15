68c730260605d09c352044644f8b67f61cd85bfd
Joel Weiser <weiserj@reactomerelease.(none)>
2015-12-15 14:50:00 -0500

    about reactome version 55

--- website/html/wordpress/wp-content/themes/HS_OICR_2013/front-page.php
+++ website/html/wordpress/wp-content/themes/HS_OICR_2013/front-page.php
@@ -14,7 +14,7 @@
 
     <div class="grid_5 omega">
 
-    <a class="button icon cytoscape" href="http://wiki.reactome.org/index.php/Reactome_FI_Cytoscape_Plugin"><span>Reactome FI Network</span></a>
+    <a class="button icon cytoscape" href="http://wiki.reactome.org/index.php/ReactomeFIViz"><span>Reactome FIViz app</span></a>
     </div>
   
  	 <div class="clear"></div> <!--clear the first row of buttons -->
@@ -36,7 +36,7 @@
     <div class="contentwrap">
     <div class="contenthead">About Reactome</div><!--close contenthead-->
     <div class="contentbody">
-    	Reactome is a free, open-source, curated and peer reviewed pathway database. Our goal is to provide intuitive bioinformatics tools for the visualization, interpretation and analysis of pathway knowledge to support basic research, genome analysis, modeling, systems biology and education. The current version (v54) of Reactome was released on September 30, 2015.
+    	Reactome is a free, open-source, curated and peer reviewed pathway database. Our goal is to provide intuitive bioinformatics tools for the visualization, interpretation and analysis of pathway knowledge to support basic research, genome analysis, modeling, systems biology and education. The current version (v55) of Reactome was released on December 15, 2015.
     </div><!--close contentbody-->
     </div><!--close contentwrap-->
     
@@ -47,7 +47,7 @@
     <a href="http://www.ebi.ac.uk/" target="_new"><img src="<?php bloginfo('template_directory'); ?>/images/logos/EMBL_EBI_Logo_black.png" height="40"></a>
     <br>
     </br>
-    <p>The development of Reactome is supported by a grant from the US National Institutes of Health (P41 HG003751), Ontario Research Fund, and the European Molecular Biology Laboratory.</p>
+    <p>The development of Reactome is supported by grants from the US National Institutes of Health (P41 HG003751 and 1U54GM114833-01), Ontario Research Fund, and the European Molecular Biology Laboratory.</p>
     </div><!--close credits-->    </div><!--close grid15-->
     <div class="grid_9"> 
             <div class="contentwrap">
@@ -58,7 +58,7 @@
             
             <div class="announcement">
 
-            <a href="/version-54-released/"> Current Version: Reactome V54             <!--This is a placeholder for static announcements. For example, downtime due to server maintenance, or some other important announcement-->
+            <a href="/version-55-released/"> Current Version: Reactome V55             <!--This is a placeholder for static announcements. For example, downtime due to server maintenance, or some other important announcement-->
             </div><!--close annoucement-->
 
             <!--twitter feed --><div class="twitter">
