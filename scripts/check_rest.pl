cd59879a54946d35c3aace696892ffcd64481fa1
Sheldon McKay <sheldon.mckay@gmail.com>
2016-01-05 09:44:34 -0500

    Adding check REST script

---
+++ scripts/check_rest.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl 
+use common::sense;
+use HTTP::Tiny;
+use JSON;
+
+use constant HOST => 'http://localhost/';
+
+die "Sorry, root permission required.\n" unless $> == 0;
+
+my $url = HOST . "ReactomeRESTfulAPI/RESTfulWS/frontPageItems/homo+sapiens";
+
+my $response = HTTP::Tiny->new->get($url);
+
+my $stamp = timestamp();
+
+if ($response->{success}) {
+
+    my $content = $response->{content};
+    my $json = decode_json($content);
+    my $OK = $json->[0]->{dbId};
+
+    #say $content;
+
+    my $stamp = timestamp();
+
+    if ($OK) {
+	say "OK $stamp";
+    }
+    else {
+	say STDERR "Error, restarting $stamp";
+	say $content;
+	system "/etc/init.d/tomcat7 restart";
+    }
+}
+else {
+    say STDERR "Error, restarting $stamp";
+    system "/etc/init.d/tomcat7 restart";
+}
+
+sub timestamp {
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);
+    return sprintf ( "%04d-%02d-%02d:%02d:%02d:%02d",
+                                   $year+1900,$mon+1,$mday,$hour,$min,$sec);
+}
