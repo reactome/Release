bbb42a7e4d106b95d01bcc6fecaf8467e0684b9a
Sheldon McKay <sheldon.mckay@gmail.com>
2016-01-24 17:58:22 -0500

    adding stable right after slice; also switch to myISAM

--- slicingTool/runProjectSlicingTool.sh
+++ slicingTool/runProjectSlicingTool.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # Make sure we have all of the necessary params
-LOG=$(grep    logFileName slicingTool.prop | perl -pe 's/^\S+=//');
-DB=$(grep   slicingDbName slicingTool.prop | perl -pe 's/^\S+=//');
-USER=$(grep slicingDbUser slicingTool.prop | perl -pe 's/^\S+=//');
-PASS=$(grep  slicingDbPwd slicingTool.prop | perl -pe 's/^\S+=//');
-
+LOG=$(grep    logFileName slicingTool.prop | grep -v '\#' |perl -pe 's/^\S+=//');
+DB=$(grep   slicingDbName slicingTool.prop | grep -v '\#' |perl -pe 's/^\S+=//');
+USER=$(grep slicingDbUser slicingTool.prop | grep -v '\#' |perl -pe 's/^\S+=//');
+PASS=$(grep  slicingDbPwd slicingTool.prop | grep -v '\#' |perl -pe 's/^\S+=//');
+PREV_DB=$(grep previousSlicingDbName slicingTool.prop | grep -v '\#' |perl -pe 's/^\S+=//');
+VER=$(grep releaseNumber slicingTool.prop | perl -pe 's/^\S+=//');
 if [[ -n $DB && -n $USER && -n $PASS ]]
 then
     if [[ $USER =~ "curator" ]]
     then
 	echo Dropping $DB ...
 	mysql -u$USER -p$PASS -e "drop database if exists $DB"
-	mysql -u$USER -p$PASS -e "drop database if exists ${DB}_myisam"
-	mysql -u$USER -p$PASS -e "create database ${DB}_myisam"
-	mysql -u$USER -p$PASS -e "drop database if exists gk_central"
-	mysql -u$USER -p$PASS -e "create database gk_central"
+#	mysql -u$USER -p$PASS -e "drop database if exists ${DB}_myisam"
+#	mysql -u$USER -p$PASS -e "create database ${DB}_myisam"
+#	mysql -u$USER -p$PASS -e "drop database if exists gk_central"
+#	mysql -u$USER -p$PASS -e "create database gk_central"
     else
 	echo "I was expecting the database user to be 'curator'.  Please edit slicingTool.prop and try again"
     fi
@@ -51,16 +52,9 @@ done
 
 killall -9 tail >/dev/null 2>&1
 
-echo "
-Now converting the database to MyISAM tables...
-"
-mysqldump -u$USER -p$PASS ${DB} | perl -pe 's/InnoDB/MyISAM/' | mysql -u$USER -p$PASS ${DB}_myisam
-mysqldump -u$USER -p$PASS ${DB}_myisam | perl -pe 's/InnoDB/MyISAM/' | mysql -u$USER -p$PASS $DB
-mysql  -u$USER -p$PASS -e "drop database ${DB}_myisam"
+echo "Adding new stable IDs..."
+./add_stable_ids.pl -sdb $DB -gdb gk_central -pdb $PREV_DB -ghost localhost -user $USER -pass $PASS -release $VER>>$LOG
 
-#echo "
-#Now installing the slice database
-#"
 
 #/usr/local/gkb/scripts/install_slice_db.pl $DB
 
