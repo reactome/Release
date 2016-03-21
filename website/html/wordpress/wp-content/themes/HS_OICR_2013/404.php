78820613b20425c0e2bbf747d8094c7cbb76016e
Joel Weiser <weiserj@reactomerelease.(none)>
2016-03-21 11:25:47 -0400

    wordpress updates

---
+++ website/html/wordpress/wp-content/themes/HS_OICR_2013/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+	<div id="content">
+
+		<h2 class="center">This resource is currently unavailable -- we are working to restore it</h2>
+
+	</div>
+
+<?php get_sidebar(); ?>
