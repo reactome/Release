5e5ac1404c2b54f7833388ac89773f12e925729d
Joel Weiser <weiserj@reactomerelease.(none)>
2016-03-28 18:16:23 -0400

    fix for commit stats files step

--- scripts/release/commit_stats_files/commit_stats_files.pl
+++ scripts/release/commit_stats_files/commit_stats_files.pl
@@ -4,11 +4,15 @@ use strict;
 use autodie qw/:all/;
 use Getopt::Long;
 
-my ($version);
-GetOptions("version:s" => \$version);
-$version || die "Usage: $0 -version reactome_version";
+my ($version, $statistic_file_directory);
+GetOptions(
+    "version:s" => \$version,
+    'stats_file_dir:s' => \$statistic_file_directory
+);
+$version || die "Usage: $0 -version reactome_version [-stats_file_dir statistics_file_directory_path]";
+$statistic_file_directory ||= '/usr/local/gkb/scripts/release/website_files_update';
 
 my @statistic_files = qw/stats.html stats.png inference_stats.png report_ortho_inference.txt release_stats/;
-system("git add $_") foreach @statistic_files;
+system("git add $statistic_file_directory/$_") foreach @statistic_files;
 system("git commit -m 'version $version'");
 system("git push");
