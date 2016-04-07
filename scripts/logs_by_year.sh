b8972b7a7c9d4557279d1f11a9f0e9cbe8f811b4
Sheldon McKay <sheldon.mckay@gmail.com>
2016-04-07 15:59:06 -0400

    shell script to to web log analysis

---
+++ scripts/logs_by_year.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+echo "changing to the apache log directory..."
+cd /usr/local/gkb/website/logs
+
+# Get the logs for specified year
+year=$1
+
+if [[ ! -n $year ]]
+then
+    echo "You must tell which year you want: $0 year"
+    exit 1
+fi
+
+logs=$year\_logs
+
+mkdir -p $year\_logs
+
+echo "Getting log data for year $year..."
+grep "/$year" transfer_log | grep -vi 'js\|json\|css' > $logs/$year\_transfer_log.txt
+grep "/$year" extended_log | grep -vi 'js\|json\|css'> $logs/$year\_extended_log.txt
+
+cd $logs
+
+# get the Analysis Service hits
+echo "Processing Analysis Service..."
+grep '/AnalysisService' $year\_transfer_log.txt > $year\_AnalysisService.txt
+echo completed $year\_AnalysisService.txt
+
+# get the counted, unique IPs
+grep -v 'content/detail' $year\_AnalysisService.txt | cut -f1 -d' ' | sort | uniq -c | sort -nr > \
+$year\_AnalysisService_hits_by_ip.txt
+echo completed $year\_AnalysisService_hits_by_ip.txt
+
+
+# Break down the Analysis Service hits
+perl -pe 's/^.+AnalysisService\/([^\/ \%]+).+$/$1/' $year\_AnalysisService.txt | \
+perl -pe 's!.+/AnalysisService/?.+$!/!' | grep -v  'content/detail' | sort  | \
+uniq -c |sort -nr  > $year\_AnalysisService_breakdown.txt
+echo completed $year\_AnalysisService_breakdown.txt
+
+
+# Now RESTful API
+echo Processing RESTful API...
+grep RESTful $year\_extended_log.txt  | grep -v '/pages\|content\|wordpress' > $year\_REST.txt
+echo completed $year\_REST.txt
+
+# get number of hits by IP
+cut -f1 -d' ' $year\_REST.txt | sort | uniq -c | sort -nr > $year\_REST_hits_by_ip.txt
+echo completed $year\_REST_hits_by_ip.txt
+
+
+# get breakdown
+perl -pe 's!^.+(/ReactomeRESTfulAPI(/RESTfulWS/)?[\/A-Za-z]*)\b.+!$1!' $year\_REST.txt | \
+sort |uniq -c | sort -nr > $year\_REST_breakdown.txt
+echo completed $year\_REST_breakdown.txt
+
+# get external accesses
+grep -v PathwayBrowser $year\_REST.txt > $year\_REST_external.txt
+echo completed $year\_REST_external.txt
+
+
+# get external breakdown
+perl -pe 's!^.+(/ReactomeRESTfulAPI(/RESTfulWS/)?[\/A-Za-z]*)\b.+!$1!' $year\_REST_external.txt | \
+sort |uniq -c | sort -nr > $year\_REST_external_breakdown.txt
+echo completed $year\_REST_external_breakdown.txt
+
+# get external hits
+cut -f1 -d' ' $year\_REST_external.txt | sort | uniq -c | sort -nr > $year\_REST_external_hits_by_ip.txt   
+echo completed $year\_REST_external_hits_by_ip.txt
+
+
+# Get searches
+grep ' /content' $year\_transfer_log.txt > $year\_search.txt
+echo completed $year\_search.txt
+
+# get breakdown
+perl -pe 's/^.+(\/content\/[A-Za-z]+)\b.+$/$1/' $year\_search.txt | 
+perl -pe 's/^.+\/content\/?\s+.+$/\//' | sort | uniq -c | sort -nr > $year\_search_breakdown.txt
+echo completed $year\_search_breakdown.txt
+
+cut -f1 -d' ' $year\_search.txt | sort | uniq -c | sort -nr > $year\_search_hits_by_ip.txt
+echo completed $year\_search_hits_by_ip.txt
+
+echo Done!
+exit
