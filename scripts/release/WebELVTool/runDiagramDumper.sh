8bf7520730e04e9de28896bc270791f5d80bcd43
Joel Weiser <weiserj@reactomerelease.(none)>
2016-06-21 10:48:58 -0400

    added java headless parameter

--- scripts/release/WebELVTool/runDiagramDumper.sh
+++ scripts/release/WebELVTool/runDiagramDumper.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx2048m -cp webELVTool.jar:mysql.jar:itext-2.0.4.jar org.gk.pathwaylayout.PathwayDiagramDumper $1 $2 $3 $4 $5 $6
+java -Djava.awt.headless=true -Xmx2048m -cp webELVTool.jar:mysql.jar:itext-2.0.4.jar org.gk.pathwaylayout.PathwayDiagramDumper $1 $2 $3 $4 $5 $6
