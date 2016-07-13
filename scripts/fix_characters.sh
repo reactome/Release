1917b6d7e1e4d86a481b48526c96f4c5350575b4
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-07-13 16:27:10 -0400

    Include the character sequence fixer in the slicing process
    Also: update the character sequence bash script to accept the database host as a parameter.

--- scripts/fix_characters.sh
+++ scripts/fix_characters.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
 # First argument to this script should be the name of the database.
-# Second argument should be either "true" or "false" - this will indicate if the script will actually execute updates.
+# Third argument to this script should be the host of the database.
+# Fourth argument should be either "true" or "false" - this will indicate if the script will actually execute updates.
 # Output will be a log file with date-formatted name: fix_chars_<YYYYMMDD_HH24mmss_Z>.log
 
 DATABASE_NAME=$1
-DO_UPDATE=$2
+DATABASE_HOST=$2
+DO_UPDATE=$3
 DATE_STR=$(date +%Y%m%d_%H%M%S_%Z)
-mysql --default-character-set=utf8 -D $DATABASE_NAME -u root -e "SET @run_update = $DO_UPDATE; `cat $(pwd)/generic_fix_chars_proc.sql`" > fix_chars_$DATE_STR.log
+mysql --default-character-set=utf8 -D $DATABASE_NAME -h $DATABASE_HOST -u root -e "SET @run_update = $DO_UPDATE; `cat $(pwd)/generic_fix_chars_proc.sql`" > fix_chars_$DATE_STR.log
