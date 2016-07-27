c0646be39f5df82172bea96e28a3b8e305ce0ba5
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-07-27 11:45:27 -0400

    Update install script.
    - fix some bugs
    - download fireworks and diagram files
    - install solr

--- third_party_install/install_reactome.sh
+++ third_party_install/install_reactome.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# A script to automate the installation of reactome 
-# software and data on an Ubuntu or Debian server
-# Sheldon McKay <sheldon.mckay@gmail.com>
+##############################
+# Install basic dependencies #
+##############################
+
+# Abort the script if anything fails.
+set -e
+MROOT=$1
 
 # this value should be interpolated at release time
 RELEASE=RELEASENUM
@@ -12,8 +16,6 @@ then
     echo -e "\nSorry, you must be the root user run this script!" 1>&2
     exit 1
 fi
-
-MROOT = $1
 
 if [[ ! -n $MROOT ]]
 then
@@ -38,8 +40,6 @@ then
     HAVEMYSQL=1
 fi
 
-APT=$(command -v apt-get)
-
 if [[ -n $APT ]] 
 then
     echo -e "\nThis is probably Debian/Ubuntu Linux"
@@ -51,23 +51,23 @@ sleep 1
 
 PWD=$(pwd)
 
-if [[ -n $APT ]]
-then
-    echo -e "Installing system software for Debian/Ubuntu..."
-    apt-get clean  -yq
-    apt-get update -yq
+sleep 1
+echo -e "Installing system software for Debian/Ubuntu..."
+apt-get clean  -yq
+apt-get update -yq
 
-    # mysql to be install non-interactively
-    export DEBIAN_FRONTEND=noninteractive
-    if [[ ! -n $HAVEMYSQL ]]
-    then
+# mysql to be install non-interactively
+export DEBIAN_FRONTEND=noninteractive
+if [[ ! -n $HAVEMYSQL ]] then
 	apt-get install -yq mysql-server
-    fi
+fi
 
-    apt-get install -yq \
+# Install other dependencies
+apt-get install -yq \
 	build-essential \
 	perl \
 	curl \
+	cpanminus \
 	apache2 \
 	php5 \
 	php5-mysql \
@@ -76,39 +76,25 @@ then
 	libexpat1 \
 	libexpat1-dev \
 	libgd-gd2-perl \
-        libbio-perl-perl
-fi
-
-# cgi scripts look here for perl
-cd /usr/local/bin
-ln -s /usr/bin/perl
-cd $PWD
-
-# We will use the cpanminus app to bring in the perl modules
-if [[ ! -n $(command -v cpanm) ]]
-then
-    echo -e "\nInstalling APP::cpanminus";
-    curl -L http://cpanmin.us | perl - --sudo App::cpanminus
-fi
-
-echo -e "\nInstalling perl modules..."
+    libbio-perl-perl \
+	wget
 
 cpanm -q \
-HTTP::Tiny \
-IO::String \
-LWP::UserAgent \
-MIME::Lite \
-Net::OpenSSH \
-XML::Simple \
-Search::Tools \
-Capture::Tiny \
-WWW::SearchResult \
-JSON \
-PDF::API2 \
-Log::Log4perl \
-common::sense \
-Email::Valid \
-URI::Encode
+		HTTP::Tiny \
+		IO::String \
+		LWP::UserAgent \
+		MIME::Lite \
+		Net::OpenSSH \
+		XML::Simple \
+		Search::Tools \
+		Capture::Tiny \
+		WWW::SearchResult \
+		JSON \
+		PDF::API2 \
+		Log::Log4perl \
+		common::sense \
+		Email::Valid \
+		URI::Encode
 
 echo -e "\nDownloading the reactome software..."
 mkdir -p /usr/local/reactomes/Reactome/production
@@ -123,78 +109,113 @@ rm -f reactome.tar.gz
 cd /usr/local
 ln -s /usr/local/reactomes/Reactome/production/GKB gkb
 
-cd /usr/local/gkb/website/html/download
-ln -sf $RELEASE current
+###########################
+# Get necessary           #
+# fireworks/diagram files #
+###########################
+echo "getting fireworks files"
+mkdir -p /usr/local/reactomes/Reactome/production/GKB/website/html/download/current/fireworks
+cd /usr/local/reactomes/Reactome/production/GKB/website/html/download/current/fireworks
+wget -nd -nv -r --no-parent http://reactome.org/download/current/fireworks/
+rm index.html* robots.txt
 
+echo "getting diagram files"
+mkdir -p /usr/local/reactomes/Reactome/production/GKB/website/html/download/current/diagram
+cd /usr/local/reactomes/Reactome/production/GKB/website/html/download/current/diagram
+# this is a big download, so -q option might be nice here
+wget -nd -nv -r --no-parent http://reactome.org/download/current/diagram/
+rm index.html* robots.txt
+
+####################
+# Database section #
+####################
 echo -e "Updating configuration..."
 cd /
 tar zxvf /usr/local/gkb/third_party_install/config.tar.gz
 rm -fr /usr/local/gkb/third_party_install
 
-cd $PWD
+cd /usr/local/gkb/website/html/download
+
 echo -e "\nDownloading the reactome databases..."
 
 mkdir databases
 cd databases
-wget http://www.reactome.org/download/current/databases/gk_current.sql.gz
-wget http://www.reactome.org/download/current/databases/gk_stable_ids.sql.gz
-wget http://www.reactome.org/download/current/databases/gk_wordpress.sql.gz
-wget http://www.reactome.org/download/current/databases/gk_current_dn.sql.gz
+wget -nv -t 5 http://www.reactome.org/download/current/databases/gk_current.sql.gz
+wget -nv -t 5 http://www.reactome.org/download/current/databases/gk_stable_ids.sql.gz
+wget -nv -t 5 http://www.reactome.org/download/current/databases/gk_wordpress.sql.gz
+# wget http://www.reactome.org/download/current/databases/gk_current_dn.sql.gz
+service mysql start
 
-if [[ ! $HAVEMYSQL && -n $MROOT ]]
-then
-    mysqladmin -u root password $MROOT
-fi
+mysqladmin -u root password $MROOT
 
-if [[ -n $MROOT ]]
-then
-   MROOT="--password=$MROOT"
-fi
-
-mysql -uroot $MROOT -e 'DROP DATABASE IF EXISTS gk_current'
-mysql -uroot $MROOT -e 'CREATE DATABASE gk_current'
-mysql -uroot $MROOT -e 'DROP DATABASE IF EXISTS gk_current_dn'
-mysql -uroot $MROOT -e 'CREATE DATABASE gk_current_dn'
-mysql -uroot $MROOT -e 'DROP DATABASE IF EXISTS stable_identifiers'
-mysql -uroot $MROOT -e 'CREATE DATABASE stable_identifiers'
-mysql -uroot $MROOT -e 'DROP DATABASE IF EXISTS gk_wordpress'
-mysql -uroot $MROOT -e 'CREATE DATABASE gk_wordpress'
+service  mysql restart
+mysql -uroot -p$MROOT -e 'DROP DATABASE IF EXISTS gk_current'
+mysql -uroot -p$MROOT -e 'CREATE DATABASE gk_current'
+mysql -uroot -p$MROOT -e 'DROP DATABASE IF EXISTS gk_current_dn'
+mysql -uroot -p$MROOT -e 'CREATE DATABASE gk_current_dn'
+mysql -uroot -p$MROOT -e 'DROP DATABASE IF EXISTS stable_identifiers'
+mysql -uroot -p$MROOT -e 'CREATE DATABASE stable_identifiers'
+mysql -uroot -p$MROOT -e 'DROP DATABASE IF EXISTS gk_wordpress'
+mysql -uroot -p$MROOT -e 'CREATE DATABASE gk_wordpress'
 
 
 echo -e "\nLoading main reactome database..."
-zcat gk_current.sql.gz | mysql -uroot $MROOT gk_current
+zcat gk_current.sql.gz | mysql -uroot -p$MROOT gk_current
 rm -f gk_current.sql.gz
 
 echo -e "\nLoading simplified reactome database..."
-zcat gk_current_dn.sql.gz | mysql -uroot $MROOT gk_current_dn
+zcat gk_current_dn.sql.gz | mysql -uroot -p$MROOT gk_current_dn
 rm -f gk_current.sql.gz
 
 
 echo -e "\nLoading reactome_wordpress database..."
-zcat gk_wordpress.sql.gz | mysql -uroot $MROOT gk_wordpress
+zcat gk_wordpress.sql.gz | mysql -uroot -p$MROOT gk_wordpress
 rm -f gk_wordpress.sql.gz
 
 echo -e "\nLoading main reactome_stable_identifiers database..."
-zcat gk_stable_ids.sql.gz | mysql -uroot $MROOT stable_identifiers
+zcat gk_stable_ids.sql.gz | mysql -uroot -p$MROOT stable_identifiers
 rm -f gk_stable_ids.sql.gz
 
 echo -e "\nSetting database permissions..."
-mysql -uroot $MROOT -e "GRANT SELECT ON stable_identifiers.* \
+mysql -uroot -p$MROOT -e "GRANT SELECT ON stable_identifiers.* \
 TO 'reactome_user'@'localhost' IDENTIFIED BY 'reactome_pass'"
-mysql -uroot $MROOT -e "GRANT SELECT ON gk_current.*  \
+mysql -uroot -p$MROOT -e "GRANT SELECT ON gk_current.*  \
 TO 'reactome_user'@'localhost' IDENTIFIED BY 'reactome_pass'"
-mysql -uroot $MROOT -e "GRANT SELECT ON gk_current_dn.* \
+mysql -uroot -p$MROOT -e "GRANT SELECT ON gk_current_dn.* \
 TO 'reactome_user'@'localhost' IDENTIFIED BY 'reactome_pass'"
-mysql -uroot $MROOT -e "GRANT ALL ON gk_wordpress.* \
+mysql -uroot -p$MROOT -e "GRANT ALL ON gk_wordpress.* \
 TO 'reactome_user'@'localhost' IDENTIFIED BY 'reactome_pass'"
 
 # reset the worpress permalinks so they will work on a new server
-mysql -uroot $MROOT gk_wordpress -e "update wp_options set option_value=NULL where option_name = 'permalink_structure'"
+mysql -uroot -p$MROOT gk_wordpress -e "update wp_options set option_value=NULL where option_name = 'permalink_structure'"
 
 rm -fr databases*
 
-echo -e "Setting up Apache..."
+#######################
+# Get and execute     #
+# Solr install script #
+#######################
 
+APACHE=apache2
+
+# set up solr
+echo -e "\nSetting up Solr..."
+mkdir -p /usr/local/gkb/scripts/
+cd /usr/local/gkb/scripts/
+echo "Getting Solr installer..."
+wget https://raw.githubusercontent.com/reactome/Search/master/install_solr.sh
+echo "Executing Solr installer..."
+chmod a+x /usr/local/gkb/scripts/install_solr.sh
+apt-get install unzip
+/usr/local/gkb/scripts/install_solr.sh -i reactome_pass
+
+rm /usr/local/reactomes/Reactome/production/apache-tomcat/conf/Catalina/localhost/solr.xml
+chown -R solr:solr /usr/local/reactomes/Reactome/production/Solr
+
+#####################
+# Web server setup. #
+#####################
+echo -e "Setting up Apache..."
 cd /usr/local/gkb/website/html
 mkdir img-fp
 mkdir img-tmp
@@ -216,7 +237,9 @@ cd ../sites-available
 ln -s /usr/local/gkb/website/conf/httpd.conf reactome.conf
 cd ../sites-enabled
 a2ensite reactome
-a2dissite default
+service apache2 start
+service apache2 reload
+#a2dissite default
 rm -f *default*
 
 # are we apache > 2.4?  # New syntax
@@ -240,22 +263,15 @@ chown -R tomcat7:tomcat7 AnalysisService Solr RESTful
 cd $PWD
 
 echo -e "\nStarting the tomcat server..."
-/etc/init.d/tomcat7 restart
+/etc/init.d/tomcat7 restart || (ls -l /usr/local/reactomes/Reactome/production/apache-tomcat/logs/ && head -n 50 /usr/local/reactomes/Reactome/production/apache-tomcat/logs/catalina.out)
 
 echo -e "\nStarting the apache web server..."
 /etc/init.d/$APACHE restart
-
-
-# set up solr
-echo -e "\nSetting up Solr..."
-/usr/local/gkb/scripts/install_solr.sh -i reactome_pass
-
 
 # auto start on reboot
 echo -e "\nSetting up automatic tomcat start on boot"
 sudo update-rc.d tomcat7 defaults
 
 echo -e "Done Installation!\n"
-
+set +e
 exit 0
-
