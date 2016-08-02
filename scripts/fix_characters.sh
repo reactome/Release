f54afc413ed05aa7064a08b8692c113eb68654e4
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-08-02 14:21:32 -0400

    Fix output formatting.

--- scripts/fix_characters.sh
+++ scripts/fix_characters.sh
@@ -9,4 +9,4 @@ DATABASE_NAME=$1
 DATABASE_HOST=$2
 DO_UPDATE=$3
 DATE_STR=$(date +%Y%m%d_%H%M%S_%Z)
-mysql --default-character-set=utf8 -D $DATABASE_NAME -h $DATABASE_HOST -u root -e "SET @run_update = $DO_UPDATE; `cat $(pwd)/generic_fix_chars_proc.sql`" > fix_chars_$DATE_STR.log
+mysql --default-character-set=utf8 --table -D $DATABASE_NAME -h $DATABASE_HOST -u root -e "SET @run_update = $DO_UPDATE; `cat $(pwd)/generic_fix_chars_proc.sql`" > fix_chars_$DATE_STR.log
