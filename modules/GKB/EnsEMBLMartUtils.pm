52615079de82f45314df1f2d34a31a2470442730
Joel Weiser <weiserj@reactomerelease.(none)>
2016-11-21 13:42:57 -0500

    new method to get gene protein mapping

--- modules/GKB/EnsEMBLMartUtils.pm
+++ modules/GKB/EnsEMBLMartUtils.pm
@@ -4,9 +4,12 @@ use strict;
 use warnings;
 
 use Carp;
+use Capture::Tiny ':all';
 use Cwd 'abs_path';
 use File::Basename 'dirname';
+use Time::Out qw/timeout/;
 use Try::Tiny;
+
 
 use lib "/usr/local/reactomes/Reactome/development/GKB/modules";
 use lib '/usr/local/reactomes/Reactome/development/GKB/BioMart/biomart-perl/lib';
@@ -15,15 +18,78 @@ use BioMart::Initializer;
 use BioMart::Query;
 use BioMart::QueryRunner;
 
+use GKB::Config_Species;
 use GKB::EnsEMBLUtils qw/:all/;
 use parent 'GKB::EnsEMBLUtils';
 
-our @EXPORT_OK = qw/get_query get_query_runner get_registry update_registry_file get_identifiers/;
+our @EXPORT_OK = qw/get_species_results get_query get_query_runner get_registry update_registry_file get_identifiers/;
 push @EXPORT_OK, @GKB::EnsEMBLUtils::EXPORT_OK;
 
 our %EXPORT_TAGS = (all => [@EXPORT_OK],
                    query => [qw/get_query get_query_runner/]);
 
+sub get_species_results {
+    my $species = shift;
+    
+    my $species_dataset = $species_info{$species}->{'mart_group'};
+    my $species_virtual_schema = $species_info{$species}->{'mart_virtual_schema'} || 'default';
+    my $species_mart_url  = $species_info{$species}->{'mart_url'} || 'http://www.ensembl.org/biomart/martservice';
+    
+    my $species_results;
+    my $results_complete;
+    my $query_attempts = 0;
+    until (($species_results && $results_complete) || $query_attempts == 3) {
+        $query_attempts += 1;
+        
+        my $five_minutes = 5 * 60;
+        timeout $five_minutes => sub {
+            $species_results = capture_stdout {
+                system(get_wget_query($species_mart_url, $species_dataset, $species_virtual_schema));
+            };
+
+            if ($species_results =~ /ERROR/) {
+                $species_results = capture_stdout {
+                    system(get_wget_query($species_mart_url, $species_dataset, $species_virtual_schema, 'uniprot_swissprot_accession'));
+                };
+            }
+        };
+
+        $results_complete = $species_results =~ /\[success\]$/;
+    }
+    
+    return $species_results;
+}
+
+sub get_wget_query {
+    my $mart_url = shift;
+    my $mart_dataset = shift;
+    my $mart_virtual_schema = shift;
+    my $swissprot_attribute_name = shift;
+    
+    return "wget -q -O - '$mart_url?query=" . get_xml_query($mart_dataset, $mart_virtual_schema, $swissprot_attribute_name) . "'";
+}
+
+sub get_xml_query {
+    my ($dataset, $virtual_schema, $swissprot_attribute_name) = @_;
+    
+    $dataset // confess "No dataset defined\n";
+    $virtual_schema // confess "No virtual schema defined\n";
+    $swissprot_attribute_name //= "uniprot_swissprot";
+    
+    return <<XML;
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE Query>
+<Query  virtualSchemaName = "$virtual_schema" formatter = "TSV" header = "0" uniqueRows = "0" count = "" completionStamp = "1">	
+	<Dataset name = "$dataset" interface = "default" >
+		<Attribute name = "ensembl_gene_id" />
+		<Attribute name = "$swissprot_attribute_name" />
+		<Attribute name = "uniprot_sptrembl" />
+        <Attribute name = "ensembl_peptide_id" />
+	</Dataset>
+</Query>
+XML
+}
+                   
 sub get_registry {
     my $action = shift // 'cached';
     my $registry_file = shift;
