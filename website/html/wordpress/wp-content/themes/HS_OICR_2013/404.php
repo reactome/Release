504ad0086eebbb6df43cbc841de64b216b5d1435
Sheldon McKay <sheldon.mckay@gmail.com>
2017-02-01 14:40:01 -0500

    replaces deprecated method

--- website/html/wordpress/wp-content/themes/HS_OICR_2013/404.php
+++ website/html/wordpress/wp-content/themes/HS_OICR_2013/404.php
@@ -2,7 +2,7 @@
 
 	<div id="content">
 
-		<h2 class="center">This resource is currently unavailable -- we are working to restore it</h2>
+		<h2 class="center">The page requested is unavailable</h2>
 
 	</div>
 
