ce0e19a68cbf7b0dcd6da12a2652b5a0e22bc9ab
Joel Weiser <weiserj@reactomerelease>
2017-09-18 12:42:31 -0400

    fixed white space

--- modules/GKB/Release/Steps/GenerateOrthoInferenceStableIds.pm
+++ modules/GKB/Release/Steps/GenerateOrthoInferenceStableIds.pm
@@ -3,36 +3,37 @@ package GKB::Release::Steps::GenerateOrthoInferenceStableIds;
 use GKB::Release::Config;
 use GKB::Release::Utils;
 
+use GKB::NewStableIdentifiers;
+
 use Moose;
 extends qw/GKB::Release::Step/;
 
 has '+gkb' => ( default => "gkbdev" );
 has '+passwords' => ( default => sub { ['mysql'] } );
 has '+directory' => ( default => "$release/generate_stable_ids_orthoinference" );
-has '+mail' => ( default => sub { 
-					my $self = shift;
-					return {
-						'to' => 'curation',
-						'subject' => $self->name,
-						'body' => "",
-						'attachment' => ""
-					};
-				}
-);
+has '+mail' => ( default => sub {
+                    my $self = shift;
+                    return {
+                        'to' => 'curation',
+                        'subject' => $self->name,
+                        'body' => "",
+                        'attachment' => ""
+                    };
+});
 
 override 'run_commands' => sub {
     my ($self, $gkbdir) = @_;
     
     $self->cmd("Backing up $db and $stable_id_db databases",
-    	[
+        [
             ["mysqldump --opt -u$user -p$pass $db > $db.beforeOrthoStableIDs.dump"],
-    	    ["mysqldump --opt -u$user -p$pass $stable_id_db > $stable_id_db\_$version.beforeOrthoStableIDs.dump"]
-    	]
+            ["mysqldump --opt -u$user -p$pass $stable_id_db > $stable_id_db\_$version.beforeOrthoStableIDs.dump"]
+        ]
     );
     
     $self->cmd("Generating stable ids for orthoinferences",
-    	[
-    	    ["perl add_ortho_stable_ids.pl -user $user -pass $pass -db $db -sdb $slicedb -release_num $version " .
+        [
+            ["perl add_ortho_stable_ids.pl -user $user -pass $pass -db $db -sdb $slicedb -release_num $version " .
              " > generate_stable_ids_$version.ortho.out 2> generate_stable_ids_$version.ortho.err"]
         ]
     );
@@ -40,21 +41,21 @@ override 'run_commands' => sub {
     $self->cmd("Saving stable ids to history database",
         [
             ["perl save_stable_id_history.pl -db $db -sdb $stable_id_db -user $user -pass $pass -release $version " .
-	    " > save_stable_id_history_$version.out 2> save_stable_id_history_$version.err"]
+            " > save_stable_id_history_$version.out 2> save_stable_id_history_$version.err"]
         ]
-	);
+    );
 
     $self->cmd("Mapping old ST_IDs back to current set",
-	       [
+        [
             ["perl old_stable_id_mapping.pl -db $db > old_stable_id_mapping_$version.out 2> old_stable_id_mapping_$version.err"]
         ]
     );
 
     $self->cmd("Backing up $db and stable_identifiers databases",
-	[
-	    ["mysqldump --opt -u$user -p$pass $db > $db.afterOrthoStableIDs.dump"],
-	    ["mysqldump --opt -u$user -p$pass $stable_id_db > $stable_id_db\_$version.afterOrthoStableIDs.dump"]
-	]
+        [
+            ["mysqldump --opt -u$user -p$pass $db > $db.afterOrthoStableIDs.dump"],
+            ["mysqldump --opt -u$user -p$pass $stable_id_db > $stable_id_db\_$version.afterOrthoStableIDs.dump"]
+        ]
     );
 };
 
@@ -71,6 +72,7 @@ override 'post_step_tests' => sub {
     return grep { defined } (
         super(),
         $self->pre_step_tests(),
+        get_stable_id_QA_problems_as_list_of_strings(get_dba($db)),
         _check_stable_id_count($db, "test_reactome_$prevver")
     );
 };
