c039946d14fda2d2f79305bd1318dc77f89608bf
Joel Weiser <weiserj@reactomerelease>
2017-09-29 15:54:46 -0400

    added gk_current update for dev server

--- modules/GKB/Release/Steps/UpdateGkCurrent.pm
+++ modules/GKB/Release/Steps/UpdateGkCurrent.pm
@@ -23,30 +23,40 @@ has '+mail' => ( default => sub {
 override 'run_commands' => sub {
     my ($self, $gkbdir) = @_;
 
-    $self->cmd("Dumping $db",[["mysqldump -u$user -p$pass $db > $db.dump"]]);
+    $self->cmd("Creating source file from $db on localhost",[["mysqldump -u$user -p$pass $db > $db.dump"]]);
  
     my @args = ("-db", 'gk_current', "-source", "$db.dump");
-    $self->cmd("Populating gk_current with $db.dump",[["perl restore_database.pl @args > gk_current.out 2> gk_current.err"]]);
+    $self->cmd("Populating gk_current with $db.dump",[["perl restore_database.pl @args >> gk_current.out 2>> gk_current.err"]]);
+    
+    $self->cmd("Backing up gk_current hosted on $dev_server",[["mysqldump -u$user -p$pass -h $dev_server gk_current > gk_current.dev.dump"]]);
+    $self->cmd("Populating gk_current on $dev_server with $db.dump",[["perl restore_database.pl @args -host $dev_server >> gk_current.out 2>> gk_current.err"]]);
 };
 
 override 'post_step_tests' => sub {
     my ($self) = shift;
     
     my @errors = super();
-    push @errors, _check_database_object_counts_are_equal($db, 'gk_current');
+    
+    my $error = _check_database_object_counts_are_equal(get_dba($db), get_dba('gk_current'));
+    push @errors, $error if $error;
+    $error = _check_database_object_counts_are_equal(get_dba($db), get_dba('gk_current', $dev_server));
+    push @errors, $error if $error;
     
     return @errors;
 };
 
 sub _check_database_object_counts_are_equal {
-    my $db = shift;
-    my $gk_current = shift;
+    my $first_dba = shift;
+    my $second_dba = shift;
 
-    my $db_object_count = get_dba($db)->class_instance_count('DatabaseObject');
-    my $gk_current_object_count = get_dba($gk_current)->class_instance_count('DatabaseObject');
+    my $first_count = $first_dba->class_instance_count('DatabaseObject');
+    my $second_count = $second_dba->class_instance_count('DatabaseObject');
     
-    return "Database object count for $gk_current ($gk_current_object_count) is different from $db ($db_object_count)"
-    unless ($db_object_count == $gk_current_object_count);
+    if ($first_count == $second_count) {
+        return;
+    }
+    
+    return "Database object count for " . $first_dba->{'db_name'} . " ($first_count) is different from " . $second_dba->{'db_name'} . " ($second_count)";
 }
 
 1;
