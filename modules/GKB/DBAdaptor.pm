cf9528a10018f6ab4ed4e48083745d777a325d2d
Joel Weiser <weiserj@reactomerelease>
2017-10-11 16:08:16 -0400

    check value is defined

--- modules/GKB/DBAdaptor.pm
+++ modules/GKB/DBAdaptor.pm
@@ -2565,7 +2565,7 @@ sub fetch_multiple_attributes_values {
     foreach my $i (0 .. $#{$order}) {
 	ref($order->[$i]) || $self->throw("Not an array ref: '$order->[$i]'.");
 	$order{$order->[$i]->[0]}->{'order'} = $i;
-	$order{$order->[$i]->[0]}->{'desc'} = 1 if (uc($order->[$i]->[1]) eq 'DESC');
+	$order{$order->[$i]->[0]}->{'desc'} = 1 if (defined $order->[$i]->[1] && uc($order->[$i]->[1]) eq 'DESC');
     }
     my (@select,%from,@where,@order);
     my $db_id_name = $GKB::Ontology::DB_ID_NAME;
