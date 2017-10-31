8bbc671de5e6408806e5bab4e39be294c7d709d1
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-10-31 16:20:57 -0400

    Save databases from container in $(pwd)/mysql_databases

--- container/run_database_container.sh
+++ container/run_database_container.sh
@@ -5,8 +5,9 @@ RELEASE_NUMBER=$2
 PREV_RELEASE_NUMBER=$3
 # TODO: clean up to have more generic names of database files - i.e. get rid of things like "20171024" in db name.
 docker run --name release_database --net reactome_release -p 3306:3306 \
- 	-v $PATH_TO_DATABASE_ROOT/gk_central_reactomerelease_20171024.sql:/docker-entrypoint-initdb.d/gk_central.sql \
-	-v $PATH_TO_DATABASE_ROOT/test_slice_${RELEASE_NUMBER}.sql:/docker-entrypoint-initdb.d/test_slice_${RELEASE_NUMBER}.sql \
+    -v $PATH_TO_DATABASE_ROOT/gk_central_reactomerelease_20171024.sql:/docker-entrypoint-initdb.d/gk_central.sql \
+    -v $PATH_TO_DATABASE_ROOT/test_slice_${RELEASE_NUMBER}.sql:/docker-entrypoint-initdb.d/test_slice_${RELEASE_NUMBER}.sql \
     -v $PATH_TO_DATABASE_ROOT/test_slice_${PREV_RELEASE_NUMBER}.sql:/docker-entrypoint-initdb.d/test_slice_${PREV_RELEASE_NUMBER}.sql \
-	-v $PATH_TO_DATABASE_ROOT/stable_identifiers_20171024.sql:/docker-entrypoint-initdb.d/stable_identifiers.sql \
-	-e MYSQL_ROOT_PASSWORD=root  mysql
+    -v $PATH_TO_DATABASE_ROOT/stable_identifiers_20171024.sql:/docker-entrypoint-initdb.d/stable_identifiers.sql \
+    -v $(pwd)/mysql_databases:/var/lib/mysql \
+    -e MYSQL_ROOT_PASSWORD=root  release_dbs
