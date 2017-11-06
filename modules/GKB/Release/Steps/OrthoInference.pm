065b7d79855afdb8744eb293681dffda1f183583
Solomon Shorser <Solomon.Shorser@oicr.on.ca>
2017-11-06 17:08:11 -0500

    Fixed braces.

--- modules/GKB/Release/Steps/OrthoInference.pm
+++ modules/GKB/Release/Steps/OrthoInference.pm
@@ -20,10 +20,10 @@ has '+mail' => ( default => sub {
                             'attachment' => ""
                         };
 });
-has '+user_input' => (default => sub {
-                    {'skip_list_verified' => {'query' => "Has the normal event skip list been verified for version $version (y/n):"}},
-                    {'release_date' => {'query' => "Enter release date for version $version as yyyy-mm-dd (e.g. 2017-12-13):" }}
-});
+has '+user_input' => (default => sub { {
+                    'skip_list_verified' => {'query' => "Has the normal event skip list been verified for version $version (y/n):"},
+                    'release_date' => {'query' => "Enter release date for version $version as yyyy-mm-dd (e.g. 2017-12-13):" }
+} });
 
 override 'run_commands' => sub {
     my ($self, $gkbdir) = @_;
