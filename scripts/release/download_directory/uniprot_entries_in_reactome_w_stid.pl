cc3528451476aad960e59bc64d4be9a8a164058c
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2018-04-19 16:21:11 -0400

    Begin work on changing Regulation.regulatedEntity

--- scripts/release/download_directory/uniprot_entries_in_reactome_w_stid.pl
+++ scripts/release/download_directory/uniprot_entries_in_reactome_w_stid.pl
@@ -39,7 +39,6 @@ my %instructions1 = (-INSTRUCTIONS =>
 			$protein_class => {'reverse_attributes' => [qw(referenceEntity)]},
 			'CatalystActivity' => {'reverse_attributes' =>[qw(catalystActivity)]},
 #			'PhysicalEntity' => {'reverse_attributes' => [qw(hasComponent input output physicalEntity hasMember hasCandidate regulator)]},
-#			'Regulation' => {'attributes' =>[qw(regulatedEntity)]}
 			'PhysicalEntity' => {'reverse_attributes' => [qw(hasComponent input output physicalEntity hasMember hasCandidate repeatedUnit)]}
 		    },
 		    -OUT_CLASSES => ['Event']
