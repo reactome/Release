29c1d25f7262802330530a3e0eef2fac911b7ef6
Justin Cook <cookersjs@gmail.com>
2018-04-25 13:45:41 -0400

    Removed comment about regulatedEntity

--- modules/GKB/DocumentGeneration/ReactomeDatabaseReader.pm
+++ modules/GKB/DocumentGeneration/ReactomeDatabaseReader.pm
@@ -1544,12 +1544,7 @@ sub get_regulation_text_units_from_instance()
 	my ($self, $instance) = @_;
 	my @text_units = ();
 	my $text_unit;
-
-	# TODO: This will need to change. Regulation.regulatedEntity will be removed. Instead, ReactionlikeEvent will have a new 'regulatedBy' attribute.
-	# This function is expecting something that is an input/output of a ReactionlikeEvent as the function input. Maybe we should be looking up the regulator
-	# rather than doing a reverse lookup of regulatedEntity? Or do a regular lookup on regulatedBy, except that will need the original event, not its
-	# inputs/outputs.
-#	my $regulators = $instance->reverse_attribute_value('regulatedEntity');
+  
 	my $regulators = $instance->regulatedBy;
 	if (!(defined $regulators) || scalar(@{$regulators}) < 1) {
 		return @text_units;
