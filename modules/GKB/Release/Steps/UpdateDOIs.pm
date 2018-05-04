34e5397bf5497f29f734e5e8cf12b0d0021e4315
Justin Cook <cookersjs@gmail.com>
2018-05-04 11:50:04 -0400

    Revert "Update DOIs converted to java and implemented into release script"
    This reverts commit 2d2a83399f4638b1af04d8add51ad9a35fa6b78f.

--- modules/GKB/Release/Steps/UpdateDOIs.pm
+++ modules/GKB/Release/Steps/UpdateDOIs.pm
@@ -8,9 +8,9 @@ extends qw/GKB::Release::Step/;
 
 has '+gkb' => ( default => "gkbdev" );
 has '+passwords' => ( default => sub { ['mysql'] } );
-# has '+user_input' => ( default => sub { {'live_run' => {'query' => 'Is this DOI update a live run -- i.e. databases to be changed? (y/n):'}}});
+has '+user_input' => ( default => sub { {'live_run' => {'query' => 'Is this DOI update a live run -- i.e. databases to be changed? (y/n):'}}});
 has '+directory' => ( default => "$release/update_dois" );
-has '+mail' => ( default => sub {
+has '+mail' => ( default => sub { 
 					my $self = shift;
 					return {
 						'to' => 'curation',
@@ -24,21 +24,18 @@ has '+mail' => ( default => sub {
 override 'run_commands' => sub {
     my ($self, $gkbdir) = @_;
 
+    my $host = $self->host;
+
     $self->cmd("Backing up databases $db and $gkcentral",
         [
             ["mysqldump -u$user -p$pass -h$host --lock-tables=FALSE $db > $db.dump"],
             ["mysqldump -u$user -p$pass -h$gkcentral_host --lock-tables=FALSE $gkcentral > $gkcentral.dump"]
         ]
     );
-
-    # my $live_run = $self->user_input->{'live_run'}->{'response'} =~ /^y/i ? '-live_run' : '';
-    # my @args = ("-user", $user, "-pass", $pass, "-release_db", $db, "-release_db_host", $host, "-curator_db", $gkcentral, "-curator_db_host", $gkcentral_host, $live_run);
-    $self->cmd("Running script to update DOIs for $db and $gkcentral",
-			[
-				["mvn clean compile assembly:single"],
-				["java -jar target/update-dois-0.0.1-jar-with-dependencies.jar"]
-			]
-		);
+    
+    my $live_run = $self->user_input->{'live_run'}->{'response'} =~ /^y/i ? '-live_run' : '';
+    my @args = ("-user", $user, "-pass", $pass, "-release_db", $db, "-release_db_host", $host, "-curator_db", $gkcentral, "-curator_db_host", $gkcentral_host, $live_run);
+    $self->cmd("Running script to update DOIs for $db and $gkcentral",[["perl update_dois.pl @args > update_dois.out 2>> update_dois.err"]]);
 };
 
 1;
