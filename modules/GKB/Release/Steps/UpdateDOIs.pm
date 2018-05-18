5cab77be72e9f6bfae80e2607742837dc3da97ac
Justin Cook <cookersjs@gmail.com>
2018-05-18 15:45:21 -0400

    Added some logic for deciding if the repo should be updated/cloned, and a reminder that the user should provide some config files

--- modules/GKB/Release/Steps/UpdateDOIs.pm
+++ modules/GKB/Release/Steps/UpdateDOIs.pm
@@ -8,7 +8,13 @@ extends qw/GKB::Release::Step/;
 
 has '+gkb' => ( default => "gkbdev" );
 has '+passwords' => ( default => sub { ['mysql'] } );
-has '+user_input' => ( default => sub { {'live_run' => {'query' => 'Is this DOI update a live run -- i.e. databases to be changed? (y/n):'}}});
+# has '+user_input' => ( default => sub { {'live_run' => {'query' => 'Is this DOI update a live run -- i.e. databases to be changed? (y/n):'}}});
+has '+user_input' => (default => sub {
+                    {
+                        'props' => {'query' => 'Is the proper config.properties file and UpdateDOIs.report(optional) in the update_dois directory?:'},
+                        'clone' => {'query' => 'Does data-release-pipeline need to be updated/cloned?:'}
+                    }
+});
 has '+directory' => ( default => "$release/update_dois" );
 has '+mail' => ( default => sub {
 					my $self = shift;
@@ -25,7 +31,6 @@ override 'run_commands' => sub {
     my ($self, $gkbdir) = @_;
 
     my $host = $self->host;
-
     $self->cmd("Backing up databases $db and $gkcentral",
         [
             ["mysqldump -u$user -p$pass -h$host --lock-tables=FALSE $db > $db.dump"],
@@ -35,7 +40,8 @@ override 'run_commands' => sub {
 
     # my $live_run = $self->user_input->{'live_run'}->{'response'} =~ /^y/i ? '-live_run' : '';
     # my @args = ("-user", $user, "-pass", $pass, "-release_db", $db, "-release_db_host", $host, "-curator_db", $gkcentral, "-curator_db_host", $gkcentral_host, $live_run);
-    $self->cmd("Running script to update DOIs for $db and $gkcentral",[["perl setup_update_dois.pl > setup_update_dois.out 2>> setup_update_dois.err"]]);
+		my $update = $self->user_input->{'clone'}->{'response'} =~ /^y|yes/i ? '-clone' : '';
+		$self->cmd("Running script to update DOIs for $db and $gkcentral",[["perl setup_update_dois.pl $update > setup_update_dois.out 2>> setup_update_dois.err"]]);
 };
 
 1;
--- scripts/release/update_dois/setup_update_dois.pl
+++ scripts/release/update_dois/setup_update_dois.pl
@@ -15,24 +15,29 @@ use Log::Log4perl qw/get_logger/;
 # Log::Log4perl->init(\$LOG_CONF);
 my $logger = get_logger(__PACKAGE__);
 
-if (-e "data-release-pipeline") {
+my $arg;
+if ($ARGV[0] && $ARGV[0] eq '-clone') {
+  $arg = $ARGV[0];
+}
+
+if (-e "data-release-pipeline" || !$arg) {
   chdir "data-release-pipeline/update-dois";
 } else {
   clone_update_dois_repo();
+  chdir "data-release-pipeline/update-dois";
 }
-chdir "data-release-pipeline/update-dois";
+
 build_jar_and_execute();
 
 sub clone_update_dois_repo {
   my $resource_dir = "data-release-pipeline/update-dois/src/main/resources/";
   system("git clone https://github.com/reactome/data-release-pipeline");
   system("cp config.properties $resource_dir");
-  system("cp log4j.properties $resource_dir");
+  # system("cp log4j.properties $resource_dir");
   system("cp UpdateDOIs.report $resource_dir");
 }
 
 sub build_jar_and_execute {
   system("mvn clean compile assembly:single");
   system("java -jar target/update-dois-0.0.1-jar-with-dependencies.jar");
-  system("cp UpdateDOIs.log ../..");
 }
