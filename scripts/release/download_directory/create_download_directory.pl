b8dcc0821bbdea31e74a28e16f146751afc69ebe
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2018-06-06 17:04:29 -0400

    No more WordPress.

--- scripts/release/download_directory/create_download_directory.pl
+++ scripts/release/download_directory/create_download_directory.pl
@@ -197,19 +197,7 @@ my @cmds = (
      "mkdir $release_nr/databases",
      "mysqldump --opt $mysqldump_db_options | gzip -c > $release_nr/databases/gk_current.sql.gz",
      "mysqldump --opt $mysqldump_identifier_db_options | gzip -c > $release_nr/databases/gk_stable_ids.sql.gz",
-     "mysqldump --opt $mysqldump_wordpress_db_options  > $release_nr/databases/gk_wordpress.sql",
      "mysqldump --opt $mysqldump_dn_db_options | gzip -c > $release_nr/databases/gk_current_dn.sql.gz",
-     # Next we need to sanitize the WordPress dump. So we will restore it to a temporary database, sanitzie it to remove user's names and passwords,
-     # and then dump *that* as the final database.
-     "mysql  -u $opt_user -h $opt_host  -p$opt_pass -P $opt_port -e 'CREATE DATABASE tmp_wordpress IF NOT EXISTS' ",
-     "mysql --opt $mysqldump_tmp_wordpress_db_options < $release_nr/databases/gk_wordpress.sql",
-     # execute the sanitize script
-     "mysql --opt $mysqldump_tmp_wordpress_db_options < ./sanitize_wordpress.sql",
-     # remove the original dump file
-     "rm $release_nr/databases/gk_wordpress.sql",
-     # dump the sanitized database to a file
-     "mysqldump --opt $mysqldump_tmp_wordpress_db_options | gzip -c >  $release_nr/databases/gk_wordpress.sql.gz",
-     "mysql  -u $opt_user -h $opt_host  -p$opt_pass -P $opt_port -e 'DROP DATABASE tmp_wordpress IF EXISTS' "
     ],
 
     [
