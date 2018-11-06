2a58b905b3fa48aba3966c827512b0709919cb0a
Joel Weiser <weiserj@reactomerelease.oicr.on.ca>
2018-11-06 13:51:15 -0500

    changed to get non-EWAS sets in catalyst activities

--- scripts/release/goa_prepare/sets_as_physical_entities.pl
+++ scripts/release/goa_prepare/sets_as_physical_entities.pl
@@ -7,7 +7,7 @@ use GKB::CommonUtils;
 
 use autodie;
 use Getopt::Long;
-use List::MoreUtils qw/all any/;
+use List::MoreUtils qw/any notall/;
 
 # Database connection
 our($host, $db, $help);
@@ -24,34 +24,35 @@ my $dba = get_dba($db, $host);
 (my $output_file_name = $0) =~ s/\.pl$//;
 open(my $output, ">", "$output_file_name.txt");
 
-my @catalyst_activities_with_sets = grep { physical_entity_is_an_EWAS_set($_) || active_unit_is_an_EWAS_set($_) } @{$dba->fetch_instance(-CLASS => 'CatalystActivity')};
-print $output join("\t", qw/Catalyst_Activity Set Members/) . "\n";
+my @catalyst_activities_with_sets = grep { physical_entity_is_a_non_EWAS_set($_) || active_unit_is_a_non_EWAS_set($_) } @{$dba->fetch_instance(-CLASS => 'CatalystActivity')};
+print $output join("\t", qw/Species Catalyst_Activity_ID Catalyst_Activity_Name Set_ID Set_Name Members/) . "\n";
 foreach my $catalyst_activity (@catalyst_activities_with_sets) {
     my @sets;
-    push @sets, $catalyst_activity->physicalEntity->[0] if physical_entity_is_an_EWAS_set($catalyst_activity);
-    push @sets, grep { is_an_EWAS_set($_) } @{$catalyst_activity->activeUnit} if active_unit_is_an_EWAS_set($catalyst_activity);
+    push @sets, $catalyst_activity->physicalEntity->[0] if physical_entity_is_a_non_EWAS_set($catalyst_activity);
+    push @sets, grep { is_a_non_EWAS_set($_) } @{$catalyst_activity->activeUnit} if active_unit_is_a_non_EWAS_set($catalyst_activity);
     
     foreach my $set (@sets) {
-        print $output join("\t", (get_name_and_id($catalyst_activity), get_name_and_id($set), join('|', map { get_name_and_id($_) } @{$set->hasMember}))) . "\n";
+        my $species_name = $set->species->[0] ? $set->species->[0]->displayName : 'N/A';
+        print $output join("\t", ($species_name, $catalyst_activity->db_id, $catalyst_activity->displayName, $set->db_id, $set->displayName, join('|', map { get_name_and_id($_) } @{$set->hasMember}))) . "\n";
     }
 }
 
-sub physical_entity_is_an_EWAS_set {
+sub physical_entity_is_a_non_EWAS_set {
     my $catalyst_activity = shift;
     
-    return is_an_EWAS_set($catalyst_activity->physicalEntity->[0]);
+    return is_a_non_EWAS_set($catalyst_activity->physicalEntity->[0]);
 }
 
-sub active_unit_is_an_EWAS_set {
+sub active_unit_is_a_non_EWAS_set {
     my $catalyst_activity = shift;
     
-    return any { is_an_EWAS_set($_) } @{$catalyst_activity->activeUnit};
+    return any { is_a_non_EWAS_set($_) } @{$catalyst_activity->activeUnit};
 }
 
-sub is_an_EWAS_set {
+sub is_a_non_EWAS_set {
     my $instance = shift;
     
     return $instance &&
            $instance->is_a('EntitySet') &&
-           (all { $_->is_a('EntityWithAccessionedSequence') } @{$instance->hasMember});
+           (notall { $_->is_a('EntityWithAccessionedSequence') } @{$instance->hasMember});
 }
