1074f49e27c0b77cf3414d952784a757463f5ceb
Joel Weiser <joel.weiser@oicr.on.ca>
2018-12-20 17:03:36 -0500

    Refactoring and style changes

--- modules/GKB/Release/Steps/CommitGoa.pm
+++ modules/GKB/Release/Steps/CommitGoa.pm
@@ -6,7 +6,9 @@ use GKB::Release::Utils;
 use Moose;
 extends qw/GKB::Release::Step/;
 
-has '+gkb' => ( default => "gkbdev" );
+my $reactome_mapping_file = "Reactome2GoV$version";
+
+has '+gkb' => ( default => 'gkbdev' );
 has '+passwords' => ( default => sub { ['mysql'] } );
 has '+directory' => ( default => "$release/commit_goa" );
 has '+mail' => ( default => sub {
@@ -14,8 +16,8 @@ has '+mail' => ( default => sub {
                     return {
                         'to' => 'automation',
                         'subject' => $self->name,
-                        'body' => "",
-                        'attachment' => "Reactome2GoV$version"
+                        'body' => '',
+                        'attachment' => $reactome_mapping_file
                     };
                 }
 );
@@ -26,7 +28,7 @@ override 'run_commands' => sub {
     my $host = $self->host;
 
     my $go_submission = "$release/goa_prepare/GO_submission/go/gene-associations/submission/gene_association.reactome";
-    $self->cmd("Committing gene association file to GO SVN",
+    $self->cmd('Committing gene association file to GO SVN',
         [
             ["rm -f $go_submission"],
             ["mv $release/goa_prepare/gene_association.reactome $go_submission"],
@@ -35,9 +37,16 @@ override 'run_commands' => sub {
         ]
     );
 
-    my @args = ("-user", $user, "-pass", $pass, "-host", $host, "-db", $db, "-date", $date, "-debug");
+    my @args = (
+        '-user', $user,
+        '-pass', $pass,
+        '-host', $host,
+        '-db', $db,
+        'output_file', $reactome_mapping_file,
+        '-debug'
+    );
 
-    $self->cmd("Creating Reactome2GO file",[["perl reactome2go.pl @args > reactome2go.out 2> reactome2go.err"]]);
+    $self->cmd('Creating Reactome2GO file',[["perl reactome2go.pl @args > reactome2go.out 2> reactome2go.err"]]);
 };
 
 override 'archive_files' => sub {
@@ -46,7 +55,7 @@ override 'archive_files' => sub {
     # arguments passed to this method are implicitly passed to the superclass method by Moose
     # https://metacpan.org/pod/release/DOY/Moose-2.0604/lib/Moose/Manual/MethodModifiers.pod#OVERRIDE-AND-SUPER
     my $archive_directory = super();
-    system("mv Reactome2GoV$version $archive_directory"); # Move Reactome2GO mapping file to archive
+    system "mv $reactome_mapping_file $archive_directory";
 };
 
 1;
