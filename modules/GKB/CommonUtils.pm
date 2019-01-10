57423e2080e954d4125d99252f5da463fe831f79
Joel Weiser <joel.weiser@oicr.on.ca>
2019-01-10 16:23:12 -0500

    Bug fix -- variable name

--- modules/GKB/CommonUtils.pm
+++ modules/GKB/CommonUtils.pm
@@ -274,7 +274,7 @@ sub has_human_source_instance {
 }
 
 sub fetch_curator_database_physical_entity_lookup {
-    my $database = shift // $GKB::Config::$GK_CURATOR_DB_NAME;
+    my $database = shift // $GKB::Config::GK_CURATOR_DB_NAME;
     my $host = shift // $GKB::Config::GK_CURATOR_DB_HOST;
 
     my $dba = get_dba($database, $host);
