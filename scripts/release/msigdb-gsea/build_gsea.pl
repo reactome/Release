26e20a339b84328eb7b907ad23e64682441bf60e
Joel Weiser <joel.weiser@oicr.on.ca>
2019-01-17 14:12:44 -0500

    Builds GSEAExport.jar and dependencies for MSigDB_GSEA

---
+++ scripts/release/msigdb-gsea/build_gsea.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+our $VERSION = 1.0;
+
+use autodie qw/:all/;
+use Cwd;
+
+my %repositories = (
+    PathwayExchange => {
+        url => 'https://github.com/reactome/Pathway-Exchange.git',
+        ant_xml_file => 'GSEAdeployAsApplication.xml',
+        output => 'GSEAExport.jar'
+    },
+    CuratorTool => {
+        url => 'https://github.com/reactome/CuratorTool.git',
+        ant_xml_file => 'WebELVDiagram.xml',
+        output => 'WebELVTool/*.jar'
+    }
+);
+
+foreach my $repository_name (keys %repositories) {
+    my $current_dir = getcwd;
+    if (-d "$repository_name/.git") {
+
+        chdir $repository_name;
+        system 'git pull';
+        chdir $current_dir;
+    } else {
+        my $repository_url = $repositories{$repository_name}{'url'};
+        system "git clone $repository_url";
+    }
+
+    chdir "$repository_name/ant";
+    my $ant_xml_file = $repositories{$repository_name}{'ant_xml_file'};
+    system "ant -buildfile $ant_xml_file";
+    chdir $current_dir;
+
+    my $output = $repositories{$repository_name}{'output'};
+    system "mv $repository_name/$output .";
+}
