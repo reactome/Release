af7cc92c1aec8c473ff77269ba69ebcdb29dc903
Joel Weiser <joel.weiser@oicr.on.ca>
2019-01-21 17:21:56 -0500

    Calls script to build GSEA jar and dependencies before running MSigDB

--- modules/GKB/Release/Steps/MSigDB_GSEA.pm
+++ modules/GKB/Release/Steps/MSigDB_GSEA.pm
@@ -6,15 +6,15 @@ use GKB::Release::Utils;
 use Moose;
 extends qw/GKB::Release::Step/;
 
-has '+gkb' => ( default => "gkbdev" );
+has '+gkb' => ( default => 'gkbdev' );
 has '+passwords' => ( default => sub { ['mysql'] } );
 has '+directory' => ( default => "$release/msigdb-gsea" );
-has '+mail' => ( default => sub { 
+has '+mail' => ( default => sub {
 					my $self = shift;
 					return {
 						'to' => '',
 						'subject' => $self->name,
-						'body' => "",
+						'body' => '',
 						'attachment' => "Reactome_GeneSet_$version.txt"
 					};
 				}
@@ -22,8 +22,23 @@ has '+mail' => ( default => sub {
 
 override 'run_commands' => sub {
     my ($self, $gkbdir) = @_;
-    
-    $self->cmd("Running GSEA output script", [["./runGSEAOutput.sh $self->{host} $db $user $pass 3306 Reactome_GeneSet_$version.txt 48887 true > msigdb-gsea.out 2> msigdb-gsea.err"]]);
+
+    $self->cmd('Build GSEA jar and dependencies', [['perl build_gsea.pl']]);
+
+    my $port = 3306;
+    my $human_species = 48887;
+    my @gsea_args = (
+        $self->{host}, # database host
+        $db, # database name
+        $user, # database user
+        $pass, # database pass
+        $port, # database port
+        "Reactome_GeneSet_$version.txt", # output file
+        $human_species, # species (human = 48887)
+        'true' # isForMsigDB? (when false, GMT format used)
+    );
+
+    $self->cmd('Running GSEA output script', [["./runGSEAOutput.sh @gsea_args > msigdb-gsea.out 2> msigdb-gsea.err"]]);
 };
-    
+
 1;
