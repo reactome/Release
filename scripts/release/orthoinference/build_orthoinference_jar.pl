f76f8b8cca2d60ae531e06f2ae8368e694632d71
Joel Weiser <joel.weiser@oicr.on.ca>
2019-01-28 16:11:07 -0500

    Clones only if local repository does not exist

--- scripts/release/orthoinference/build_orthoinference_jar.pl
+++ scripts/release/orthoinference/build_orthoinference_jar.pl
@@ -6,8 +6,11 @@ use autodie qw/:all/;
 
 my $orthoinference_repository = 'data-release-pipeline';
 
-system "git clone https://github.com/reactome/$orthoinference_repository.git";
+if (! (-d "$orthoinference_repository/.git")) {
+    system "git clone https://github.com/reactome/$orthoinference_repository.git";
+}
 chdir $orthoinference_repository;
+system 'git pull';
 system 'git checkout develop';
 chdir 'release-common-lib';
 system 'mvn clean install';
