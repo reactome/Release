41be913500317b42013815fb7f341aab9d5f8205
Joel Weiser <joel.weiser@oicr.on.ca>
2019-01-29 09:47:55 -0500

    Includes commands to setup and run Java implementation of orthoinference

--- modules/GKB/Release/Steps/OrthoInference.pm
+++ modules/GKB/Release/Steps/OrthoInference.pm
@@ -5,24 +5,25 @@ use GKB::Config_Species;
 use GKB::Release::Config;
 use GKB::Release::Utils;
 
+use Carp;
 use Moose;
 extends qw/GKB::Release::Step/;
 
-has '+gkb' => ( default => "gkbdev" );
+has '+gkb' => ( default => 'gkbdev' );
 has '+passwords' => ( default => sub { ['mysql'] } );
 has '+directory' => ( default => "$release/orthoinference" );
-has '+mail' => ( default => sub { 
-                        my $self = shift;
-                        return {
-                            'to' => '',
-                            'subject' => $self->name,
-                            'body' => "",
-                            'attachment' => ""
-                        };
+has '+mail' => ( default => sub {
+    my $self = shift;
+    return {
+        'to' => '',
+        'subject' => $self->name,
+        'body' => '',
+        'attachment' => ''
+    };
 });
 has '+user_input' => (default => sub { {
-                    'skip_list_verified' => {'query' => "Has the normal event skip list been verified for version $version (y/n):"},
-                    'release_date' => {'query' => "Enter release date for version $version as yyyy-mm-dd (e.g. 2017-12-13):" }
+    'skip_list_verified' => {'query' => "Has the normal event skip list been verified for version $version (y/n):"},
+    'release_date' => {'query' => "Enter release date for version $version as yyyy-mm-dd (e.g. 2017-12-13):" }
 } });
 
 override 'run_commands' => sub {
@@ -30,19 +31,21 @@ override 'run_commands' => sub {
 
     my $skip_list_verified = $self->user_input->{'skip_list_verified'}->{'response'} =~ /^y/i;
     if (!$skip_list_verified) {
-        die "Skip list must be verified before running the orthoinference process";
+        croak "Skip list must be verified before running the orthoinference process\n";
     }
 
     my $release_date = $self->user_input->{'release_date'}->{'response'};
     if (!$release_date || $release_date !~ /^\d{4}-\d{2}-\d{2}$/) {
-        die "Release date for version $version needed as yyyy-mm-dd";
+        croak "Release date for version $version needed as yyyy-mm-dd\n";
     }
 
-    $self->cmd("Creating orthopredictions and backing up database",
+    $self->cmd('Creating orthopredictions and backing up database',
         [
             ["mkdir -p $version"],
-            ["perl wrapper_ortho_inference.pl -r $version -user $user -pass $pass -port 3306 -release_date $release_date > $version/wrapper_ortho_inference.out"],
-            ["rm -f ../website_files_update/report_ortho_inference.txt"],
+            ["perl create_orthoinference_db.pl -source_db $slicedb -target_db $db"],
+            ['perl build_orthoinference_jar.pl'],
+            ['./runOrthoinference.sh'],
+            ['rm -f ../website_files_update/report_ortho_inference.txt'],
             ["ln $release/orthoinference/$version/report_ortho_inference_$db.txt ../website_files_update/report_ortho_inference.txt"],
             ["mysqldump --opt -u$user -p$pass -P3306 $db > $db\_after_ortho.dump"]
         ]
@@ -53,10 +56,10 @@ override 'post_step_tests' => sub {
     my $self = shift;
 
     my @errors = super();
-    
+
     # TODO: The algorithm needs to be changed to check only Orthoinference specific species instances in the current
     #       and previous databases.  Currently, the previous database has its post-Orthoinference species instances
-    #       (e.g. from AddLinks) counted also which leads to false reports of low species instance counts in the 
+    #       (e.g. from AddLinks) counted also which leads to false reports of low species instance counts in the
     #       current database
     #my @species_instance_count_errors = _check_orthoinferred_instance_count_for_all_species();
     #push @errors, @species_instance_count_errors if @species_instance_count_errors;
