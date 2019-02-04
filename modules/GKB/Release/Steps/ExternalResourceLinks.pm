662e324f6c45123af4397523c0bc48b5352fcb93
Joel Weiser <weiserj@ip-172-31-30-104.eu-west-2.compute.internal>
2019-02-04 19:01:32 +0000

    refactoring for Perl::Critic issues

--- modules/GKB/Release/Steps/ExternalResourceLinks.pm
+++ modules/GKB/Release/Steps/ExternalResourceLinks.pm
@@ -3,58 +3,58 @@ package GKB::Release::Steps::ExternalResourceLinks;
 use GKB::Release::Config;
 use GKB::Release::Utils;
 
-use Switch;
-
 use Moose;
 extends qw/GKB::Release::Step/;
 
-has '+gkb' => ( default => "gkbdev" );
+has '+gkb' => ( default => 'gkbdev' );
 has '+passwords' => ( default => sub { ['mysql'] } );
 has '+directory' => ( default => "$release/add_links" );
 has '+user_input' => (default => sub {
-                    {
-                        'person_id' => {'query' => 'Please enter your person instance db id:','hide_keystrokes' => 0},
-                        'configuration' => {'query' => "Please choose configuration: download only (d), insertion only (i), full (f):",'hide_keystrokes' => 0}
-                    }
+    {
+        'person_id' => {'query' => 'Please enter your person instance db id:','hide_keystrokes' => 0},
+        'configuration' => {
+            'query' => 'Please choose configuration: download only (d), insertion only (i), full (f):',
+            'hide_keystrokes' => 0
+        }
+    }
 });
-has '+mail' => ( default => sub { 
-					my $self = shift;
-					return {
-						'to' => '',
-						'subject' => $self->name,
-						'body' => "",
-						'attachment' => $self->directory . "/AddLinks/scripts/logs/addlinks.log"
-					};
-				}
-);
+has '+mail' => ( default => sub {
+    my $self = shift;
+    return {
+        'to' => '',
+        'subject' => $self->name,
+        'body' => '',
+        'attachment' => $self->directory . '/AddLinks/scripts/logs/addlinks.log'
+    };
+});
 
 override 'run_commands' => sub {
-	my ($self) = @_;
-    
+    my ($self) = @_;
+
     my $person_id = $self->user_input->{'person_id'}->{'response'};
     my $configuration = get_configuration($self->user_input->{'configuration'}->{'response'});
 
     # Backup database and run add links script
-    $self->cmd("Backup database", [["mysqldump --opt -u$user -p$pass $db > $db\_before_addlinks.dump"]]);
-    my @results = $self->cmd("Running add links script", [
-        ["perl setup_add_links.pl -user $user -pass $pass -db $db -person_id $person_id -configuration $configuration
-         > setup_add_links_$version.out 2> setup_add_links_$version.err"]
+    $self->cmd('Backup database', [["mysqldump --opt -u$user -p$pass $db > $db\_before_addlinks.dump"]]);
+    my @results = $self->cmd('Running add links script', [
+        ["perl setup_add_links.pl -user $user -pass $pass -db $db -person_id $person_id -configuration $configuration " .
+         "> setup_add_links_$version.out 2> setup_add_links_$version.err"]
     ]);
-    
+
     my $exit_code = ($results[0])->{'exit_code'};
-    # Backup the database or else drop and remake the database if the add links script fails  
+    # Backup the database or else drop and remake the database if the add links script fails
     if ($exit_code == 0) {
-    	$self->cmd("Backing up database $db",
-    		[
-    			["mysqldump --opt -u$user -p$pass $db > $db\_after_addlinks.dump"]
-    		]
-    	);
+        $self->cmd("Backing up database $db",
+            [
+                ["mysqldump --opt -u$user -p$pass $db > $db\_after_addlinks.dump"]
+            ]
+        );
     }
 };
 
 override 'post_step_tests' => sub {
     my $self = shift;
-    
+
     my @errors = super();
     push @errors, _check_referrer_count_for_all_reference_databases();
     return @errors;
@@ -62,25 +62,25 @@ override 'post_step_tests' => sub {
 
 sub get_configuration {
     my $user_selection = shift;
-    
-    my $configuration;
-    switch($user_selection) {
-        case /^d/i { $configuration = 'download_only' }
-        case /^i/i { $configuration = 'insertion_only' }
-        case /^f/i { $configuration = 'full' }
-        else { confess "Unknown configuration" }
+
+    if ($user_selection =~ /^d/i) {
+        return 'download_only';
+    } elsif ($user_selection =~ /^i/i) {
+        return 'insertion_only';
+    } elsif ($user_selection =~ /^f/i) {
+        return 'full';
+    } else {
+        confess "Unknown configuration: $user_selection";
     }
-    
-    return $configuration;
 }
 
 sub _check_referrer_count_for_all_reference_databases {
     my @errors;
-        
+
     my %current_reference_database_referrer_count = _get_reference_database_to_referrer_count($db, \@errors);
-    my %previous_reference_database_referrer_count = _get_reference_database_to_referrer_count("test_reactome_$prevver", \@errors);
-    
-    foreach my $reference_database_name (sort keys %previous_reference_database_referrer_count) {    
+    my %previous_reference_database_referrer_count = _get_reference_database_to_referrer_count($previous_db, \@errors);
+
+    foreach my $reference_database_name (sort keys %previous_reference_database_referrer_count) {
         my $previous_referrer_count = $previous_reference_database_referrer_count{$reference_database_name};
         my $current_referrer_count = $current_reference_database_referrer_count{$reference_database_name};
         if (!$current_referrer_count) {
@@ -88,10 +88,12 @@ sub _check_referrer_count_for_all_reference_databases {
             next;
         }
         if ($current_referrer_count < $previous_referrer_count) {
-            push @errors, "$reference_database_name has fewer referrers compared to the previous release: $version - $current_referrer_count; $prevver - $previous_referrer_count";
+            push @errors, "$reference_database_name has fewer referrers compared to the previous release: " .
+                "$version - $current_referrer_count; $prevver - $previous_referrer_count";
             next;
         }
-        releaselog("$reference_database_name external links added successfully: $version - $current_referrer_count; $prevver - $previous_referrer_count");
+        releaselog("$reference_database_name external links added successfully: " .
+            "$version - $current_referrer_count; $prevver - $previous_referrer_count");
     }
     return @errors;
 }
@@ -99,13 +101,14 @@ sub _check_referrer_count_for_all_reference_databases {
 sub _get_reference_database_to_referrer_count {
     my $database_name = shift;
     my $errors = shift;
-    
+
     my %reference_database_to_referrer_count;
     foreach my $reference_database_instance (_get_reference_database_instances($database_name)) {
         my $reference_database_name = _get_name(@{$reference_database_instance->name});
         my $referrer_count = scalar @{$reference_database_instance->reverse_attribute_value('referenceDatabase')};
         if (exists $reference_database_to_referrer_count{$reference_database_name}) {
-            push @{$errors}, "There is more than one reference database instance for $reference_database_name.  Please verify its referrer count manually";
+            push @{$errors}, "There is more than one reference database instance for $reference_database_name. " .
+                'Please verify its referrer count manually';
             next;
         }
         $reference_database_to_referrer_count{$reference_database_name} = $referrer_count;
@@ -115,29 +118,37 @@ sub _get_reference_database_to_referrer_count {
 
 sub _get_reference_database_instances {
     my $database_name = shift;
-    
+
     return @{get_dba($database_name)->fetch_instance(-CLASS => 'ReferenceDatabase')};
 }
 
 sub _get_name {
     my @names = @_;
-    
-    return unless @names;
-    return $names[0] if scalar @names == 1;
+
+    if (scalar @names == 0) {
+        return;
+    }
+
+    if (scalar @names == 1) {
+        return $names[0];
+    }
+
     return _get_longest(@names);
 }
 
 # Taken from answer to http://stackoverflow.com/questions/4182010/
 sub _get_longest {
+    my @values = @_;
+
     my $max_length = -1;
     my $max_ref;
-    for (@_) {
-        if (length > $max_length) {
-            $max_length = length;
-            $max_ref = \$_;
+    foreach my $value (@values) {
+        if (length $value > $max_length) {
+            $max_length = length $value;
+            $max_ref = \$value;
         }
     }
-    return $$max_ref;
+    return ${$max_ref};
 }
 
 1;
