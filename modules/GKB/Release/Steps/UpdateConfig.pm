bc4a0d1b3f0041ad2880fca5c9f8a6b25d065034
Joel Weiser <weiserj@ip-172-31-30-104.eu-west-2.compute.internal>
2019-02-04 15:34:07 +0000

    updates to update_config_file release step to use any given database

--- modules/GKB/Release/Steps/UpdateConfig.pm
+++ modules/GKB/Release/Steps/UpdateConfig.pm
@@ -6,29 +6,30 @@ use GKB::Release::Utils;
 use Moose;
 extends qw/GKB::Release::Step/;
 
-#has '+gkb' => ();
-#has '+passwords' => ();
 has '+directory' => ( default => "$release/update_config_file" );
 has '+mail' => ( default => sub { 
-					my $self = shift;
-					return {
-						'to' => '',
-						'subject' => $self->name,
-						'body' => "",
-						'attachment' => ""
-					};
-				}
-);
-
-my $prd = prompt('Enter the previous release date as yyyymmdd:');
+    my $self = shift;
+    return {
+        'to' => '',
+        'subject' => $self->name,
+        'body' => "",
+        'attachment' => ""
+    };
+});
+has '+user_input' => ( default => sub {{
+    'last_release_date' => { 'query' => 'Enter the previous release date as yyyymmdd:' }
+}});
 
 override 'run_commands' => sub {
     my ($self, $gkbdir) = @_;
 
     my $host = $hosts{$gkbdir};
-    my $path = "/usr/local/$gkbdir/modules/GKB";
 
-    $self->cmd("Updating configuration file",[["perl updateconfig.pl -version $version -lastrelease $prd -host $host -configpath $path"]]);
+    my $last_release_date = $self->user_input->{'last_release_date'}->{'response'};
+    my $config_path = "$gkbmodules/GKB";
+    $self->cmd("Updating configuration file",[
+        ["perl updateconfig.pl -database $db -last_release $last_release_date -host $host -config_path $config_path"]
+    ]);
     
     if ($gkbdir eq "gkbdev") {	    
     	$self->mail->{'to'} = 'curation';
