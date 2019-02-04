fc60699333d700b053d3f33abd4c7c8756845197
Joel Weiser <weiserj@ip-172-31-30-104.eu-west-2.compute.internal>
2019-02-04 15:34:07 +0000

    updates to update_config_file release step to use any given database

--- scripts/release/update_config_file/updateconfig.pl
+++ scripts/release/update_config_file/updateconfig.pl
@@ -7,35 +7,41 @@ use Getopt::Long;
 
 # Author: Joel Weiser (joel.weiser@oicr.on.ca)
 # Created: February 2014
-# Purpose: Update the Reactome GKB configuration file to reference a new test_reactome_XX database and "last release" date
-#	   This script can modify the configuration file on the local server and remotely (useful for updating the fallback
-#	   and live server copies from the automation pipeline on the release server)
+# Purpose: Update the Reactome GKB configuration file to reference a new database and "last release" date
+#          This script can modify the configuration file on the local server and remotely (useful for updating the
+#          fallback and live server copies from the automation pipeline on the release server)
 
-our ($opt_host, $opt_configpath, $opt_version, $opt_lastrelease); 
+my ($host, $config_path, $database, $last_release); 
+GetOptions(
+    "host:s" => \$host,
+    "config_path:s" => \$config_path,
+    "database:s" => \$database,
+    "last_release:i" => \$last_release
+);
 
-&GetOptions("host:s", "configpath:s", "version:i", "lastrelease:i");
-
-unless ($opt_host &&
-	$opt_configpath &&
-	$opt_version && $opt_version =~ /^\d+$/ &&
-	$opt_lastrelease && $opt_lastrelease =~ /^\d{8}$/) {
-	print "Usage: perl $0 -host CONFIG_FILE_SERVER -configpath CONFIG_DIR_PATH -version RELEASE_VERSION -lastrelease LAST_RELEASE_DATE\n";
-	print "Example: perl $0 -host reactomerelease.oicr.on.ca -configpath /usr/local/gkbdev/modules/GKB -version 48 -lastrelease 20131204\n";
-	exit 1;
+unless ($host && $config_path && $database && $last_release && $last_release =~ /^\d{8}$/) {
+    print "Usage: perl $0 -host CONFIG_FILE_SERVER -config_path CONFIG_DIR_PATH -database DATABASE " .
+        "-last_release LAST_RELEASE_DATE\n";
+    print "Example: perl $0 -host reactomerelease.oicr.on.ca -config_path /usr/local/gkbdev/modules/GKB " . 
+        "-database release_current -last_release 20131204\n";
+    exit 1;
 }
 
 # Hostname of this server
 my $hostname = `hostname -f`;
 chomp $hostname;
 
-my $ssh = ($opt_host eq "localhost" || $opt_host eq $hostname) ? 0 : 1;
+my $ssh = ($host eq "localhost" || $host eq $hostname) ? 0 : 1;
 
-my $secrets = "perl -pi -e \"s/GK_DB_NAME\\s*=\\s*'\\S+';/GK_DB_NAME  = 'test_reactome_$opt_version';/\" $opt_configpath/Secrets.pm"; # change the database name to test_reactome_xx
-my $config = "perl -pi -e \"s/LAST_RELEASE_DATE = \\d{8}/LAST_RELEASE_DATE = $opt_lastrelease/\" $opt_configpath/Config.pm"; # update the last release date
+# change the database name
+my $secrets = "perl -pi -e \"s/GK_DB_NAME\\s*=\\s*'\\S+';/GK_DB_NAME  = '$database';/\" $config_path/Secrets.pm";
+
+# update the last release date
+my $config = "perl -pi -e \"s/LAST_RELEASE_DATE = \\d{8}/LAST_RELEASE_DATE = $last_release/\" $config_path/Config.pm";
 if ($ssh) {
 	$config =~ s/"/\\"/g;
 
-	`ssh $opt_host "$config"`;
+	`ssh $host "$config"`;
 } else {
 	`$secrets`;
 	`$config`;
