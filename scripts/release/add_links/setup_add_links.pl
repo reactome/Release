67aafe04a9c8781df13ca75f4690788cda8bb73a
Joel Weiser <joel.weiser@oicr.on.ca>
2019-02-12 10:46:03 -0500

    Adds subroutine for creating reports directories

--- scripts/release/add_links/setup_add_links.pl
+++ scripts/release/add_links/setup_add_links.pl
@@ -48,6 +48,7 @@ $addlinks_dir ||= "$cwd/AddLinks";
 build_jar($addlinks_dir); #if $rebuild_jar || !(-e "$addlinks_dir/target");
 create_resources_symbolic_link("$addlinks_dir/scripts");
 create_jar_file_symbolic_link("$addlinks_dir/scripts");
+create_reports_directories("$addlinks_dir/scripts");
 set_properties("$addlinks_dir/src/main/resources/db.properties",
                {"database.user" => $user, "database.password" => $pass, "database.host" => $host, "database.port" => $port, "database.name" => $db});
 set_properties("$addlinks_dir/src/main/resources/auth.properties",
@@ -62,7 +63,7 @@ run_addlinks("$addlinks_dir/scripts");
 
 sub update_addlinks_repo {
     my $addlinks_dir = shift;
-    
+
     my $current_dir = getcwd;
     chdir $addlinks_dir;
     system("git pull");
@@ -71,13 +72,13 @@ sub update_addlinks_repo {
 
 sub clone_addlinks_repo {
     my $addlinks_dir = shift;
-    
+
     system("git clone https://github.com/reactome/add-links.git $addlinks_dir");
 }
 
 sub build_jar {
     my $addlinks_dir = shift;
-    
+
     my $current_dir = getcwd;
     chdir $addlinks_dir;
     system("mvn clean package -DskipTests");
@@ -86,20 +87,30 @@ sub build_jar {
 
 sub create_resources_symbolic_link {
     my $addlinks_script_dir = shift;
-    
+
     my $current_dir = getcwd;
     chdir $addlinks_script_dir;
     system("ln -sf ../src/main/resources resources") unless (-e 'resources');
     chdir $current_dir;
 }
 
+sub create_reports_directories {
+    my $addlinks_script_dir = shift;
+
+    my $current_dir = getcwd;
+    chdir $addlinks_script_dir;
+    system("mkdir -p reports/duplicateReports");
+    system("mkdir -p reports/diffReports");
+    chdir $current_dir;
+}
+
 sub create_jar_file_symbolic_link {
     my $addlinks_script_dir = shift;
-    
+
     my $current_dir = getcwd;
     chdir $addlinks_script_dir;
     my $addlinks_jar_file = (split(/\n/, `ls ../target/AddLinks*SHADED.jar`))[0];
-    
+
     system("ln -sf $addlinks_jar_file AddLinks.jar");
     chdir $current_dir;
 }
@@ -107,7 +118,7 @@ sub create_jar_file_symbolic_link {
 sub set_properties {
     my $file_path = shift;
     my $properties = shift;
-    
+
     my %initial_properties;
     open(my $read_fh, "<", "$file_path");
     my @contents = <$read_fh>;
@@ -118,7 +129,7 @@ sub set_properties {
         $initial_properties{$key} = $value;
     }
     close($read_fh);
-     
+
     open(my $write_fh, ">", "$file_path.new");
     foreach my $property (uniq(keys %$properties, keys %initial_properties)) {
         my $value = $properties->{$property} || $initial_properties{$property};
@@ -126,26 +137,26 @@ sub set_properties {
         print $write_fh "$property=$value\n";
     }
     close($write_fh);
-    
+
     system("mv -f $file_path.new $file_path");
 }
 
 sub configure_application_context {
     my $configuration = shift;
     my $application_context_file_path = shift;
-    
+
     my $current_dir = getcwd;
     chdir dirname("$application_context_file_path");
     system("git checkout " . fileparse($application_context_file_path));
     chdir $current_dir;
-    
+
     return if $configuration eq 'full';
-    
+
     open(my $read_fh, '<', $application_context_file_path);
     local $/ = undef;
     my $file_contents = <$read_fh>;
     close($read_fh);
-    
+
     open(my $write_fh, '>', "$application_context_file_path.new");
     $file_contents =~ s/<!--.*?-->//gms;
     if ($configuration eq 'download_only') {
@@ -154,7 +165,7 @@ sub configure_application_context {
 
         my $reference_creator_filter = get_regex_for_util_list("referenceCreatorFilter");
         $file_contents =~ s/$reference_creator_filter/$1<!--$2-->$3/;
-        
+
         my $reference_databases_to_link_check = get_regex_for_util_list("referenceDatabasesToLinkCheck");
         $file_contents =~ s/$reference_databases_to_link_check/$1<!--$2-->$3/;
     } elsif ($configuration eq 'insertion_only') {
@@ -163,13 +174,13 @@ sub configure_application_context {
     }
     print $write_fh $file_contents;
     close($write_fh);
-    
+
     system("mv -f $application_context_file_path.new $application_context_file_path");
 }
 
 sub get_regex_for_util_list {
     my $id = shift;
-    
+
     my $open_tag = qr/<util:list id="$id.*?>/;
     my $close_tag = qr/<\/util:list>/;
 
@@ -178,7 +189,7 @@ sub get_regex_for_util_list {
 
 sub run_addlinks {
     my $addlinks_script_dir = shift;
-    
+
     my $current_dir = getcwd;
     chdir $addlinks_script_dir;
     system("bash run_addlinks.sh");
