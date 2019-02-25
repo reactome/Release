f1c3739e9338149dead66b44635131402aa91946
jweiser <joel.weiser@oicr.on.ca>
2019-02-25 21:43:21 +0000

    retrieve other identifiers using wget and XML queries

--- scripts/release/other_identifiers/indirectIdentifiers_for_all_species.pl
+++ scripts/release/other_identifiers/indirectIdentifiers_for_all_species.pl
@@ -1,17 +1,15 @@
-#!/usr/local/bin/perl -w
+#!/usr/bin/perl
 use strict;
+use warnings;
 
 use Data::Dumper;
 use File::Path qw/make_path/;
 use Getopt::Long;
 
-use GKB::Utils;
-use GKB::Instance;
 use GKB::Config;
+use GKB::Config_Species;
 
 $GKB::Config::NO_SCHEMA_VALIDITY_CHECK = undef;
-
-@ARGV || die("Usage: $0 -db db_name ...\n");
 
 my @params = @ARGV;
 
@@ -19,47 +17,38 @@ our ($opt_user, $opt_host, $opt_pass, $opt_port, $opt_db, $opt_debug);
 
 &GetOptions("user:s", "host:s", "pass:s", "port:i", "db:s", "debug");
 
-$opt_db || die "Need database name (-db).\n";    
+$opt_db || die "Need database name (-db).\n";
 
-# Get connection to reactome db
-my $dba = GKB::DBAdaptor->new
-    (
-     -user   => $opt_user,
-     -host   => $opt_host,
-     -pass   => $opt_pass,
-     -port   => $opt_port,
-     -dbname => $opt_db,
-#     -DEBUG => $opt_debug
-     );
-
-my @species = $dba->species_for_ref_dbs('ENSEMBL','UniProt');
-print STDERR "My species:\n", Dumper(\@species);
+print "My species:\n", Dumper(\@species);
 
 make_path('output', { mode => 0775 });
 my @cmds = (
-	qq(./retrieve_indirectIdentifiers_from_mart.pl @params),
-	qq(./indirectIdentifiers_from_mart.pl @params),
-	qq(./gene_names_from_mart.pl @params)
+    qq(./retrieve_indirectIdentifiers_from_mart.pl @params),
+    qq(./indirectIdentifiers_from_mart.pl @params),
+    qq(./gene_names_from_mart.pl @params)
 );
 
 foreach my $sp (@species) {
     foreach my $cmd (@cmds) {
-		my $tmp = "$cmd -sp '$sp'";
-		print STDERR "Command to be run: " . hide_password($tmp) . "\n";
-		system($tmp) == 0 or print(hide_password($tmp) . " failed.\n");
+        my $tmp = "$cmd -sp '$sp'";
+        print "Command to be run: " . hide_password($tmp) . "\n";
+        system($tmp) == 0 or print(hide_password($tmp) . " failed.\n");
     }
 }
 
-print STDERR "$0: no fatal errors.\n";
+print "$0: no fatal errors.\n";
 
 sub hide_password {
     my $string = shift;
-    
+
     $string =~ /-pass (.*?) /;
     my $password = $1;
+
+    if (!$password) {
+        return $string;
+    }
+
     my $asterisks = '*' x 5;
-    
     $string =~ s/-pass $password/-pass $asterisks/;
-    
     return $string;
 }
