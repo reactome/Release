a8005b0dee8a7368ab4067d097881827c42d6438
jweiser <joel.weiser@oicr.on.ca>
2019-02-26 17:31:13 +0000

    retries identifier downloads and backs up existing ones

--- scripts/release/other_identifiers/retrieve_indirectIdentifiers_from_mart.pl
+++ scripts/release/other_identifiers/retrieve_indirectIdentifiers_from_mart.pl
@@ -8,18 +8,19 @@ use GKB::Config;
 use GKB::Config_Species;
 use GKB::EnsEMBLMartUtils qw/get_identifiers get_species_mart_name get_wget_query_for_identifier/;
 
-use Data::Dumper;
+use Capture::Tiny qw/capture/;
 use Getopt::Long;
+use List::MoreUtils qw/any/;
 use Try::Tiny;
 
 use Log::Log4perl qw/get_logger/;
 Log::Log4perl->init(\$LOG_CONF);
 my $logger = get_logger(__PACKAGE__);
 
-our($species_abbr, $help);
+our($species_code, $help);
 
 &GetOptions(
-    "sp=s" => \$species_abbr,
+    "sp=s" => \$species_code,
     "help" => \$help
 );
 
@@ -28,14 +29,18 @@ if ($help) {
     exit;
 }
 
-foreach my $species_abbreviation (get_species_to_query($species_abbr)) {
+foreach my $species_abbreviation (get_species_to_query($species_code)) {
     my $species_mart_name = get_species_mart_name($species_abbreviation);
 
     foreach my $identifier (get_identifiers($species_mart_name)) {
         next if $identifier =~ /chembl|clone_based|dbass|description|ottg|ottt|ottp|shares_cds|merops|mirbase|reactome/;
 
-        my $wget_query = get_wget_query_for_identifier($species_abbreviation, $identifier);
-        system "$wget_query > output/$species_mart_name\_$identifier";
+        my $output_file = "output/$species_mart_name\_$identifier";
+        if (-s $output_file) {
+            $logger->info("$output_file already exists - creating backup");
+            system("mv -f $output_file $output_file.bak");
+        }
+        download_identifier_file($species_abbreviation, $identifier, $output_file);
     }
 }
 
@@ -45,18 +50,59 @@ foreach my $species_abbreviation (get_species_to_query($species_abbr)) {
 sub get_species_to_query {
     my $selected_species = shift;
 
+    my $logger = get_logger(__PACKAGE__);
+
     my @species_to_query;
     if ($selected_species) {
         if (any { $_ eq $selected_species } @species) {
             @species_to_query = ($selected_species);
         } else {
-            $logger->logconfess("EnsEMBL mart information is unknown for $selected_species");
+            $logger->logconfess("$selected_species species: EnsEMBL mart information is unknown");
         }
     } else {
         @species_to_query = @species;
     }
 
     return @species_to_query;
+}
+
+# Retrieves the appropriate wget query for the species and
+# external identifier and then attemps to download the
+# identifier file and save it to an output file.
+# The function will recurse on failure to re-try
+# downloading to a pre-set maximum attempt number
+sub download_identifier_file {
+    my $species_abbreviation = shift;
+    my $identifier = shift;
+    my $output_file = shift;
+    my $attempt_count = shift // 1;
+    my $max_attempts = 3;
+
+    my $logger = get_logger(__PACKAGE__);
+
+    my $wget_query = get_wget_query_for_identifier($species_abbreviation, $identifier);
+
+    $logger->info("$identifier for species $species_abbreviation: beginning download of identifier file");
+    my ($output, $error) = capture {
+        system $wget_query;
+    };
+
+    my $completion_tag = qr/\[success\]\n/;
+    if ($output =~ /$completion_tag/) {
+        $output =~ s/$completion_tag//;
+
+        open(my $fh, '>', $output_file);
+        print {$fh} $output;
+        close $fh;
+    } else {
+        my $error_message = "$identifier for species $species_abbreviation: download of identifier file not complete";
+        if ($attempt_count < $max_attempts) {
+            $logger->warn("$error_message on attempt $attempt_count - retrying");
+            download_identifier_file($species_abbreviation, $identifier, $output_file, ++$attempt_count);
+        } else {
+            $logger->error("$error_message due to $error - aborting");
+        }
+    }
 }
 
 sub usage_instructions {
