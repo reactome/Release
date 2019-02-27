546defeb828ea265126c299aeb5fd30db6ae3805
jweiser <joel.weiser@oicr.on.ca>
2019-02-27 17:16:36 +0000

    throws error in addition to logging

--- modules/GKB/EnsEMBLMartUtils.pm
+++ modules/GKB/EnsEMBLMartUtils.pm
@@ -264,7 +264,7 @@ sub get_species_mart_name {
     if ($species_name =~ /^(\w)\w+ (\w+)$/) {
         return lc("$1$2");
     } else {
-        $logger->error("Can't form species abbreviation for mart from $species_name\n");
+        $logger->log_confess("Can't form species abbreviation for mart from $species_name\n");
     }
 }
 
