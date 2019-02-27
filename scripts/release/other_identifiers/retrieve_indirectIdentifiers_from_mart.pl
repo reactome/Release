b4acc9170481b686d65ea2fe784409369c2897f8
jweiser <joel.weiser@oicr.on.ca>
2019-02-27 15:29:37 +0000

    refactoring and tests

--- scripts/release/other_identifiers/retrieve_indirectIdentifiers_from_mart.pl
+++ scripts/release/other_identifiers/retrieve_indirectIdentifiers_from_mart.pl
@@ -14,33 +14,38 @@ use List::MoreUtils qw/any/;
 use Try::Tiny;
 
 use Log::Log4perl qw/get_logger/;
-Log::Log4perl->init(\$LOG_CONF);
-my $logger = get_logger(__PACKAGE__);
 
-our($species_code, $help);
+run(@ARGV) unless caller();
 
-&GetOptions(
-    "sp=s" => \$species_code,
-    "help" => \$help
-);
+sub run {
+    Log::Log4perl->init(\$LOG_CONF);
+    my $logger = get_logger(__PACKAGE__);
 
-if ($help) {
-    print usage_instructions();
-    exit;
-}
+    our($species_code, $help);
 
-foreach my $species_abbreviation (get_species_to_query($species_code)) {
-    my $species_mart_name = get_species_mart_name($species_abbreviation);
+    &GetOptions(
+        "sp=s" => \$species_code,
+        "help" => \$help
+    );
 
-    foreach my $identifier (get_identifiers($species_mart_name)) {
-        next if $identifier =~ /chembl|clone_based|dbass|description|ottg|ottt|ottp|shares_cds|merops|mirbase|reactome/;
+    if ($help) {
+        print usage_instructions();
+        exit;
+    }
 
-        my $output_file = "output/$species_mart_name\_$identifier";
-        if (-s $output_file) {
-            $logger->info("$output_file already exists - creating backup");
-            system("mv -f $output_file $output_file.bak");
+    foreach my $species_abbreviation (get_species_to_query($species_code)) {
+        my $species_mart_name = get_species_mart_name($species_abbreviation);
+
+        foreach my $identifier (get_identifiers($species_mart_name)) {
+            next if $identifier =~ /chembl|clone_based|dbass|description|ottg|ottt|ottp|shares_cds|merops|mirbase|reactome/;
+
+            my $output_file = "output/$species_mart_name\_$identifier";
+            if (-s $output_file) {
+                $logger->info("$output_file already exists - creating backup");
+                system("mv -f $output_file $output_file.bak");
+            }
+            download_identifier_file($species_abbreviation, $identifier, $output_file);
         }
-        download_identifier_file($species_abbreviation, $identifier, $output_file);
     }
 }
 
@@ -80,6 +85,33 @@ sub download_identifier_file {
 
     my $logger = get_logger(__PACKAGE__);
 
+    my $results = run_wget_query($species_abbreviation, $identifier);
+
+    my $output = $results->{'output'};
+    my $completion_tag = qr/\[success\]\n/;
+    if ($output =~ /$completion_tag/) {
+        $output =~ s/$completion_tag//;
+        write_output_to_file($output, $output_file);
+    } else {
+        my $error_message = "$identifier for species $species_abbreviation: download of identifier file not complete";
+        if ($attempt_count < $max_attempts) {
+            $logger->warn("$error_message on attempt $attempt_count - retrying");
+            download_identifier_file($species_abbreviation, $identifier, $output_file, ++$attempt_count);
+        } else {
+            my $error = $results->{'error'};
+            $logger->error("$error_message due to $error - aborting");
+        }
+    }
+}
+
+# Runs query to EnsEMBL Mart for species and identifier
+# Returns the output and error from the wget command as a hash reference
+sub run_wget_query {
+    my $species_abbreviation = shift;
+    my $identifier = shift;
+
+    my $logger = get_logger(__PACKAGE__);
+
     my $wget_query = get_wget_query_for_identifier($species_abbreviation, $identifier);
 
     $logger->info("$identifier for species $species_abbreviation: beginning download of identifier file");
@@ -87,22 +119,22 @@ sub download_identifier_file {
         system $wget_query;
     };
 
-    my $completion_tag = qr/\[success\]\n/;
-    if ($output =~ /$completion_tag/) {
-        $output =~ s/$completion_tag//;
+    return {
+        output => $output,
+        error => $error
+    };
+}
 
-        open(my $fh, '>', $output_file);
-        print {$fh} $output;
-        close $fh;
-    } else {
-        my $error_message = "$identifier for species $species_abbreviation: download of identifier file not complete";
-        if ($attempt_count < $max_attempts) {
-            $logger->warn("$error_message on attempt $attempt_count - retrying");
-            download_identifier_file($species_abbreviation, $identifier, $output_file, ++$attempt_count);
-        } else {
-            $logger->error("$error_message due to $error - aborting");
-        }
-    }
+# Write contents of identifier mappings from EnsEMBL Mart to output file
+sub write_output_to_file {
+    my $logger = get_logger(__PACKAGE__);
+
+    my $output = shift // $logger->logconfess("Need output to write to identifier file");
+    my $output_file = shift // $logger->logconfess("Need name of output file");
+
+    open(my $fh, '>', $output_file);
+    print {$fh} $output;
+    close $fh;
 }
 
 sub usage_instructions {
@@ -120,3 +152,5 @@ queried by providing its abbreviation (e.g. hsap for Homo sapiens) with the flag
 Usage: $0 [-sp 'species abbreviation']
 END
 }
+
+1;
