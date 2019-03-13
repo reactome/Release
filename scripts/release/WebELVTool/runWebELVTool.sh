0080e4f9b6da245143b4ce5e10c0db19607aea89
jweiser <joel.weiser@oicr.on.ca>
2019-03-13 20:33:39 +0000

    determines running directory dynamically

--- scripts/release/WebELVTool/runWebELVTool.sh
+++ scripts/release/WebELVTool/runWebELVTool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /usr/local/gkb/scripts/release/orthodiagrams/WebELVTool
+DIR=$(dirname "$(readlink -f "$0")") # Directory of the script -- allows the script to invoked from anywhere
+cd $DIR
 java -Xmx1024m -jar webELVTool.jar $1 $2 $3 $4 $5 $6 $7 $8 $9
-cd -
 
 
