fbaabde3e3d04a4bc183db232d5ac8351421fab8
jweiser <joel.weiser@oicr.on.ca>
2019-04-09 18:05:27 -0400

    bug fix - log messages now append instead of overwriting

--- scripts/gk_central_backup_checker.sh
+++ scripts/gk_central_backup_checker.sh
@@ -45,16 +45,16 @@ abs() {
 }
 
 emit_and_log_info() {
-    $msg=$1
-    $log_file=$2
-    echo $msg
+    msg=$1
+    log_file=$2
+    echo "$msg" >&1
     echo "$(date) INFO $msg" >> $log_file
 }
 
 emit_and_log_error() {
-    $msg=$1
-    $log_file=$2
-    echo $msg >&2
+    msg=$1
+    log_file=$2
+    echo "$msg" >&2
     echo "$(date) ERROR $msg" >> $log_file
 }
 
@@ -79,12 +79,12 @@ database_backup_restorable() {
     zipped_db_dump=$1
     placeholder_database="test_backup_restore_$today"
     # Create placeholder database for attempting restoring of the db dump file
-    mysql -e "DROP DATABASE IF EXISTS $placeholder_database" 2> $log
-    if mysql -e "CREATE DATABASE $placeholder_database" 2> $log; then
+    mysql -e "DROP DATABASE IF EXISTS $placeholder_database" 2>> $log
+    if mysql -e "CREATE DATABASE $placeholder_database" 2>> $log; then
         # Load the db dump into the empty placeholder database
-        if zcat $zipped_db_dump | mysql $placeholder_database 2> $log; then
+        if zcat $zipped_db_dump | mysql $placeholder_database 2>> $log; then
             # Drop the placeholder database if loading was successful
-            $(mysql -e "DROP DATABASE $placeholder_database" 2> $log)
+            $(mysql -e "DROP DATABASE $placeholder_database" 2>> $log)
             # Return "true" that the database backup is restorable
             return 0;
         else
@@ -120,16 +120,16 @@ percent_difference=$(echo "scale=10;$byte_size_difference*100/$yesterdays_backup
 tolerable_percent_drop=0.1 # 0.1 percent NOT 10 percent
 if difference_too_large "$percent_difference" "$tolerable_percent_drop"; then
     msg="Difference between gk_central back-ups from today $today and yesterday $yesterday is too large.  Check $backup_dir"
-    emit_and_log_error $msg $log
+    emit_and_log_error "$msg" "$log"
 else
     msg="Difference in size between today's and yesterday's backups was acceptable: $byte_size_difference bytes"
-    emit_and_log_info $msg $log
+    emit_and_log_info "$msg" "$log"
 fi
 
 if ! database_backup_restorable $todays_backup; then
     msg="$todays_backup could not be restored locally as a database.  Check if backup was corrupted in $backup_dir"
-    emit_and_log_error $msg $log
+    emit_and_log_error "$msg" "$log"
 else
     msg="$todays_backup was successfully restored"
-    emit_and_log_info $msg $log
+    emit_and_log_info "$msg" "$log"
 fi
