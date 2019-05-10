7fe1035c039740eb61ab725c774fd34f9bb8b0a8
Joel Weiser <joel.weiser@oicr.on.ca>
2019-05-10 15:38:35 -0400

    refactors script based on perl critic suggestions

--- scripts/release/go_update/new_GO_molecular_function.pl
+++ scripts/release/go_update/new_GO_molecular_function.pl
@@ -4,19 +4,23 @@ use warnings;
 
 use autodie;
 use Data::Dumper;
+use English qw/-no_match_vars/;
 
 use lib '/usr/local/gkb/modules';
 use GKB::CommonUtils;
 
-if (!$ARGV[0] || $ARGV[0] !~ /^\d+$/) {
-    print "Usage: perl $0 [db_id of GO update instance edit]\n";
+if (!$ARGV[0] || $ARGV[0] !~ /^\d+$/msx) {
+    print "Usage: perl $PROGRAM_NAME [db_id of GO update instance edit]\n";
     exit;
 }
 
 my $gk_central_dba = get_dba('gk_central', 'curator.reactome.org');
 
 my $instance_edit = $gk_central_dba->fetch_instance_by_db_id($ARGV[0])->[0];
-my @GO_molecular_function_instances = grep {$_->is_a('GO_MolecularFunction')} @{$instance_edit->reverse_attribute_value('created')};
+my @GO_molecular_function_instances = grep {
+    $_->is_a('GO_MolecularFunction')
+} @{$instance_edit->reverse_attribute_value('created')};
+
 foreach (@GO_molecular_function_instances) {
     print join("\t", $_->db_id, $_->accession->[0], $_->displayName, $_->definition->[0]) . "\n";
 }
